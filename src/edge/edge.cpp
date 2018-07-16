3e13b15d68f1bf519fa364147596434c2c99a12a
Simon Shillaker <mail@simonshillaker.com>
2018-07-16 17:04:25 +0100

    Started adding better edge set-up

--- src/edge/edge.cpp
+++ src/edge/edge.cpp
@@ -1,15 +1,84 @@
+#include <pistache/http.h>
+#include <pistache/router.h>
 #include <pistache/endpoint.h>
 
 using namespace Pistache;
 
-struct HelloHandler : public Http::Handler {
-    HTTP_PROTOTYPE(HelloHandler)
+/**
+ * HTTP endpoint for managing function calls.
+ */
+class FunctionEndpoint {
 
-    void onRequest(const Http::Request& request, Http::ResponseWriter writer) {
-        writer.send(Http::Code::Ok, "Hello, World!");
+public:
+    FunctionEndpoint(Address addr) :
+            httpEndpoint(std::make_shared<Http::Endpoint>(addr)) {}
+
+    /**
+     * Configures the endpoint (including threading) and its routing
+     */
+    void init(int threadCount) {
+        // Configure endpoint
+        auto opts = Http::Endpoint::options()
+                .threads(threadCount)
+                .flags(Tcp::Options::InstallSignalHandler);
+
+        httpEndpoint->init(opts);
+        setupRoutes();
+    }
+
+    void start() {
+        httpEndpoint->setHandler(router.handler());
+        httpEndpoint->serve();
+    }
+
+    void shutdown() {
+        httpEndpoint->shutdown();
+    }
+
+private:
+    std::shared_ptr<Http::Endpoint> httpEndpoint;
+    Rest::Router router;
+
+    void setupRoutes() {
+        using namespace Rest;
+
+        Routes::Post(router, "/f/:user/:function", Routes::bind(&FunctionEndpoint::function, this));
+        Routes::Get(router, "/status/", Routes::bind(&FunctionEndpoint::status, this));
+    }
+
+    void function(const Rest::Request &request, Http::ResponseWriter response) {
+        auto user = request.param(":user").as<std::string>();
+        auto function = request.param(":function").as<std::string>();
+
+        bool success = false;
+
+        //TODO Put request on queue
+
+        //TODO Wait for response
+
+        if (success) {
+            response.send(Http::Code::Ok, "Success");
+        } else {
+            response.send(Http::Code::Internal_Server_Error, "Error");
+        }
+    }
+
+    void status(const Rest::Request &request, Http::ResponseWriter response) {
+        response.send(Http::Code::Ok, "Healthy \n");
     }
 };
 
 int main() {
-    Http::listenAndServe<HelloHandler>("*:8080");
+    Port port(8080);
+
+    int threadCount = 4;
+
+    Address addr(Ipv4::any(), port);
+
+    FunctionEndpoint endpoint(addr);
+
+    endpoint.init(threadCount);
+    endpoint.start();
+
+    endpoint.shutdown();
 }
