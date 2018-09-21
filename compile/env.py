595f2c7ae343e79e8661424416239507fa900b9c
Simon Shillaker <mail@simonshillaker.com>
2018-09-21 12:13:11 +0100

    Finally got libcurl building

--- compile/env.py
+++ compile/env.py
@@ -15,35 +15,37 @@ CONFIG_TARGET = "wasm32"
 COMPILER_FLAGS = [
     "--target={}".format(TARGET_TRIPLE),
     "--sysroot={}".format(SYSROOT),
-    "-fvisibility=default",
 ]
 
 COMPILER_FLAGS_STRING = " ".join(COMPILER_FLAGS)
 COMPILER_FLAGS_STRING = "\"{}\"".format(COMPILER_FLAGS_STRING)
 
+# Getting everything mapped properly here is crucial. If we end up
+# using system defaults we can get some errors that are *very* hard
+# to debug
+
 CC = join(TOOLCHAIN_ROOT, "bin", "clang")
+CPP = join(TOOLCHAIN_ROOT, "bin", "clang-cpp")
 CFLAGS = COMPILER_FLAGS_STRING
 CXX = join(TOOLCHAIN_ROOT, "bin", "clang++")
 CXXFLAGS = COMPILER_FLAGS_STRING
+LD = join(TOOLCHAIN_ROOT, "bin", "wasm-ld")
 CROSS_COMPILE = join(TOOLCHAIN_ROOT, "llvm-")
-AR = join(TOOLCHAIN_ROOT, "llvm-ar")
-
-# Options for wasm linker: run wasm-ld --help
-LD = join(TOOLCHAIN_ROOT, "wasm-ld --no-gc-sections --export-all --error-count=0")
-#LDFLAGS = "\"-v -fexport-all -fno-gc-sections -ferror-count=0\""
-
-CPP = join(TOOLCHAIN_ROOT, "bin", "clang-cpp")
+AR = join(TOOLCHAIN_ROOT, "bin", "llvm-ar")
+AS = join(TOOLCHAIN_ROOT, "bin", "llvm-ar")
+RANLIB = join(TOOLCHAIN_ROOT, "bin", "llvm-ranlib")
 
 _ENV_TUPLES = [
     ("CC", CC),
-    ("CFLAGS", CFLAGS),
     ("CPP", CPP),
+    ("CFLAGS", CFLAGS),
+    ("AR", AR),
+    ("AS", AS),
+    ("LD", LD),
+    ("RANLIB", RANLIB),
+    ("CROSS_COMPILE", CROSS_COMPILE),
     ("CXX", CXX),
     ("CXXFLAGS", CXXFLAGS),
-    ("LD", "\"{}\"".format(LD)),
-    # ("LDFLAGS", LDFLAGS),
-    # ("LD_FLAGS", LDFLAGS),
-    ("CROSS_COMPILE", CROSS_COMPILE),
 ]
 
 ENV_DICT = {e[0]: e[1] for e in _ENV_TUPLES}
