b30c2c218391b1266fa27b5b7c29bf4a9cce07a1
Simon Shillaker <mail@simonshillaker.com>
2018-10-02 11:22:26 +0100

    Playing around with libcurl

--- func/function_curl.c
+++ func/function_curl.c
@@ -1,47 +1,31 @@
 #include "faasm.h"
 
 #include <stdio.h>
-
 #include <curl/curl.h>
 
 /**
- * Example of HTTP request
+ * Example of HTTP request with libcurl
  */
 
-size_t write_data(void *buffer, size_t size, size_t nmemb, char *data)
-{
-    printf("DATA - %s\n", data);
-    return size * nmemb;
-}
-
 int exec(struct FaasmMemory *memory) {
-    printf("Before curl init\n");
-
     CURL *curl = curl_easy_init();
-    printf("Initialised curl\n");
+    if(!curl) {
+        printf("Curl initialisation failed\n");
+    }
 
-    char *url = "http://www.google.com";
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-
-    curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
-    curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "deflate");
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-    curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+    curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com");
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
     CURLcode res = curl_easy_perform(curl);
 
-    long http_code = 0;
-    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-
-    if(CURLE_OK == res) {
-        printf("%s response = %ld\n", url, http_code);
+    if(res == CURLE_OK) {
+        printf("Curl success\n");
     }
     else {
-        printf("%s response failed %ld\n", url, http_code);
+        printf("Curl failed: %s\n", curl_easy_strerror(res));
     }
 
     curl_easy_cleanup(curl);
-    curl_global_cleanup();
 
     return 0;
 }
