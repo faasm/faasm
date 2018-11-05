4ab3318857ae06ca279a3cee1f90f7b94e535191
Simon Shillaker <mail@simonshillaker.com>
2018-11-05 19:37:12 +0000

    More small tidy-up

--- lib/faasm/faasm.h
+++ lib/faasm/faasm.h
@@ -3,18 +3,6 @@
 
 #include "faasm/memory.h"
 
-// Work out if we're in a wasm cross-compile environment
-// This can be used to avoid functions being removed by DCE
-#if __clang_major__ == 8
-
-#define FAASM_EXPORT __attribute__((used)) __attribute__ ((visibility ("default")))
-#define FAASM_INLINE
-#else
-
-#define FAASM_EXPORT
-#define FAASM_INLINE static inline
-#endif
-
 namespace faasm {
     /**
     * Function for faasm functions to implement
