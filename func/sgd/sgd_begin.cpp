6a12ab2402617c6eda8e5a178c857b4566ce5d10
Simon Shillaker <shillaker@gmail.com>
2018-11-08 20:02:24 +0000

    small tweaks

--- func/sgd/sgd_begin.cpp
+++ func/sgd/sgd_begin.cpp
@@ -7,10 +7,10 @@ namespace faasm {
         // Initialise params
         SgdParams p;
         p.nWeights = 10;
-        p.nTrain = 1000;
-        p.nBatches = 50;
-        p.maxEpochs = 10;
-        p.learningRate = 0.01;
+        p.nTrain = 10000;
+        p.nBatches = 1000;
+        p.maxEpochs = 20;
+        p.learningRate = 0.001;
 
         // Set up dummy data
         setUpDummyProblem(memory, p);
