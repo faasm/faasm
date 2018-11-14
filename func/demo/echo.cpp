7c273883c2ffb51899186e7dcfc9cad2735c72d1
Simon Shillaker <mail@simonshillaker.com>
2018-11-14 20:02:07 +0000

    Removed print from echo function

--- func/demo/echo.cpp
+++ func/demo/echo.cpp
@@ -1,7 +1,5 @@
 #include "faasm/faasm.h"
 
-#include <stdio.h>
-
 /**
  * Writes the input to the output
  */
@@ -9,7 +7,6 @@ namespace faasm {
     int exec(FaasmMemory *memory) {
         long inputSize = memory->getInputSize();
         const uint8_t *input = memory->getInput();
-        printf("MEMORY IN: %s \n", input);
 
         memory->setOutput(input, inputSize);
         return 0;
