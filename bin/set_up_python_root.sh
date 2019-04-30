d7ef337970a521afc8257486d3e7f8601cb556cf
Simon Shillaker <mail@simonshillaker.com>
2019-04-30 17:24:22 +0100

    Tidy-up

--- bin/set_up_python_root.sh
+++ bin/set_up_python_root.sh
@@ -16,12 +16,12 @@ PY_RUNTIME_ROOT=${RUNTIME_ROOT}/lib/python3.7
 SITE_PACKAGES=${PY_RUNTIME_ROOT}/site-packages
 NUMPY_DIR=${PYODIDE_ROOT}/packages/numpy/build/numpy-1.15.1/install/lib/python3.7/site-packages/numpy
 
-#sudo chown -R ${USER}:${USER} ${INSTALL_DIR}
+sudo chown -R ${USER}:${USER} ${INSTALL_DIR}
 
 rm -rf ${RUNTIME_ROOT}/*
 
-#sudo mkdir -p ${RUNTIME_ROOT}
-#sudo chown -R ${USER}:${USER} ${RUNTIME_ROOT}
+sudo mkdir -p ${RUNTIME_ROOT}
+sudo chown -R ${USER}:${USER} ${RUNTIME_ROOT}
 
 # Clear out pyc and pycache files for cpython and numpy
 remove_pyc ${INSTALL_DIR}
@@ -29,15 +29,12 @@ remove_pyc ${NUMPY_DIR}
 
 # Copy everything but remove some stuff we don't want
 cp -r ${INSTALL_DIR}/* ${RUNTIME_ROOT}
-#rm ${PY_RUNTIME_ROOT}/shutil.py
 
 # Remove the actual lib file
 rm ${RUNTIME_ROOT}/lib/libpython*
 
 # Put numpy in place and remove unnecessary bits
 cp -r ${NUMPY_DIR} ${SITE_PACKAGES}/
-#rm -r ${SITE_PACKAGES}/numpy/tests
-#rm -r ${SITE_PACKAGES}/numpy/testing
 
 # Put dummy functions in place
 cp -r ${PROJ_ROOT}/python/funcs ${RUNTIME_ROOT}
