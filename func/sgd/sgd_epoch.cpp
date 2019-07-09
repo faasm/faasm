e6dd645e50ef486d820107ecd8efd2c7253ecf81
Simon Shillaker <554768+Shillaker@users.noreply.github.com>
2019-07-09 17:19:49 +0100

    Chaining emulator (#52)
    * Added function chaining natively
    
    * Formatting

--- func/sgd/sgd_epoch.cpp
+++ func/sgd/sgd_epoch.cpp
@@ -4,41 +4,42 @@
 #include "faasm/sgd.h"
 
 FAASM_MAIN_FUNC() {
-        // Load params
-        bool fullAsync = faasm::getEnvFullAsync();
-        faasm::SgdParams p = faasm::readParamsFromState(PARAMS_KEY, fullAsync);
+    // Load params
+    bool fullAsync = faasm::getEnvFullAsync();
+    faasm::SgdParams p = faasm::readParamsFromState(PARAMS_KEY, fullAsync);
 
-        // Set per-epoch memory to zero
-        faasm::zeroErrors(p);
-        faasm::zeroFinished(p);
+    // Set per-epoch memory to zero
+    faasm::zeroErrors(p);
+    faasm::zeroFinished(p);
 
-        // Reset the barrier counter
-        faasm::initCounter(BARRIER_COUNT_KEY, fullAsync);
+    // Reset the barrier counter
+    faasm::initCounter(BARRIER_COUNT_KEY, fullAsync);
 
-        // Get the epoch count
-        int epoch = faasm::getCounter(EPOCH_COUNT_KEY, p.fullAsync);
+    // Get the epoch count
+    int epoch = faasm::getCounter(EPOCH_COUNT_KEY, p.fullAsync);
 
-        // Shuffle start indices for each batch
-        int *batchNumbers = faasm::randomIntRange(p.nBatches);
+    // Shuffle start indices for each batch
+    int *batchNumbers = faasm::randomIntRange(p.nBatches);
 
-        // Chain new calls to perform the work
-        for (int w = 0; w < p.nBatches; w++) {
-            int startIdx = batchNumbers[w] * p.batchSize;
+    // Chain new calls to perform the work
+    for (int w = 0; w < p.nBatches; w++) {
+        int startIdx = batchNumbers[w] * p.batchSize;
 
-            // Make sure we don't overshoot
-            int endIdx = std::min(startIdx + p.batchSize, p.nTrain - 1);
+        // Make sure we don't overshoot
+        int endIdx = std::min(startIdx + p.batchSize, p.nTrain - 1);
 
-            // Prepare input data for the worker as a string
-            std::string args = std::to_string(w) + " " + std::to_string(startIdx) + " " + std::to_string(endIdx) + " " + std::to_string(epoch);
-            printf("Chaining sgd_step with args: %s\n", args.c_str());
+        // Prepare input data for the worker as a string
+        std::string args = std::to_string(w) + " " + std::to_string(startIdx) + " " + std::to_string(endIdx) + " " +
+                           std::to_string(epoch);
+        printf("Chaining sgd_step with args: %s\n", args.c_str());
 
-            auto inputBytes = reinterpret_cast<const uint8_t *>(args.c_str());
+        auto inputBytes = reinterpret_cast<const uint8_t *>(args.c_str());
 
-            // Call the chained function
-            faasmChainFunctionInput("sgd_step", inputBytes, args.size());
-        }
-
-        delete[] batchNumbers;
-
-        return 0;
+        // Call the chained function
+        faasmChainFunctionInput("sgd_step", inputBytes, args.size());
     }
+
+    delete[] batchNumbers;
+
+    return 0;
+}
--- src/emulator/emulator.cpp
+++ src/emulator/emulator.cpp
@@ -4,17 +4,23 @@ extern "C" {
 #include <faasm/host_interface.h>
 }
 
+#include <util/logging.h>
 #include <redis/Redis.h>
 #include <state/State.h>
+#include <thread>
 
 /**
  * C++ emulation of Faasm system
  */
 
 static std::string _user = "demo";
-static std::vector<uint8_t> _inputData = {1, 2, 3, 4, 5};
 
-void setEmulatorUser(const char* newUser) {
+// Note thread locality here to handle multiple locally chained functions
+static thread_local std::vector<uint8_t> _inputData = {1, 2, 3, 4, 5};
+static thread_local int _funcIdx = 0;
+static std::vector<std::thread> threads;
+
+void setEmulatorUser(const char *newUser) {
     _user = newUser;
 }
 
@@ -113,20 +119,41 @@ long __faasm_read_input(unsigned char *buffer, long bufferLen) {
 }
 
 int __faasm_chain_this(int idx, const unsigned char *buffer, long bufferLen) {
-    // Copy the input data
-    _inputData = std::vector<uint8_t >(buffer, buffer+bufferLen);
+    const std::shared_ptr<spdlog::logger> &logger = util::getLogger();
+    logger->debug("Chaining local function {}", idx);
 
-    // Call the function directly
-    _FaasmFuncPtr f = getFaasmFunc(idx);
-    f();
+    // Create vector to hold inputs
+    std::vector<uint8_t> inputs(buffer, buffer + bufferLen);
 
-    // TODO - return a proper function ID here
-    return 1;
+    // Spawn a thread to execute the function
+    threads.emplace_back([idx, inputs] {
+        // Set up input data for this thread (thread-local)
+        _inputData = inputs;
+        _funcIdx = idx;
+
+        // Invoke the function
+        _FaasmFuncPtr f = getFaasmFunc(idx);
+        f();
+    });
+
+    // Value returned will be thread index in array + 1
+    int nThreads = (int) threads.size();
+    return nThreads;
+}
+
+int __faasm_await_call(int messageId) {
+    int threadIdx = messageId - 1;
+
+    // Join the thread to await its completion
+    std::thread &t = threads[threadIdx];
+    t.join();
+
+    return 0;
 }
 
 int __faasm_get_idx() {
-    // TODO - how can we emulate this?
-    return 0;
+    // Relies on thread-local idx
+    return _funcIdx;
 }
 
 void __faasm_lock_state_read(const char *key) {
@@ -150,20 +177,17 @@ void __faasm_write_output(const unsigned char *output, long outputLen) {
 }
 
 int __faasm_chain_function(const char *name, const unsigned char *inputData, long inputDataSize) {
-    // TODO - do this properly
+    // This might not be possible when executing natively
     return 1;
 }
 
-int __faasm_await_call(int messageId) {
-    return 0;
-}
 
 void __faasm_snapshot_memory(const char *key) {
-
+    // This might not be possible when executing natively
 }
 
 void __faasm_restore_memory(const char *key) {
-
+    // This might not be possible when executing natively
 }
 
 void __faasm_read_config(const char *varName, char *buffer) {
