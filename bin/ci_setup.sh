24082c7b9ff8eeea86a083d711b9ffa235b7103a
Simon Shillaker <shillaker@gmail.com>
2019-07-18 15:39:03 +0100

    Separate testing out of tree build

--- bin/ci_setup.sh
+++ bin/ci_setup.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-pushd /faasm/build
+pushd /faasm/ci-build
 
-#echo "Configure out of source build"
-#cmake -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_BUILD_TYPE=Debug /usr/local/code/faasm
+echo "Configure out of source build"
+cmake -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_BUILD_TYPE=Debug /usr/local/code/faasm
 
 echo "Build codegen"
 cmake --build . --target codegen
