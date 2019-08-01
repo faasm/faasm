1767947a0654852a6e3354c72187bbb72626e4a3
Simon Shillaker <shillaker@gmail.com>
2019-08-01 10:07:19 +0100

    Removed parallelism from build

--- bin/ci_setup.sh
+++ bin/ci_setup.sh
@@ -9,15 +9,15 @@ echo "Configure out of source build"
 cmake -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_BUILD_TYPE=Debug /usr/local/code/faasm
 
 echo "Build codegen"
-cmake --build . --target codegen  -- -j
+cmake --build . --target codegen
 
 echo "Run codegen on wasm files"
-#./bin/codegen /usr/local/code/faasm/wasm/demo
-#./bin/codegen /usr/local/code/faasm/wasm/errors
-#./bin/codegen /usr/local/code/faasm/wasm/python
-#./bin/codegen /usr/local/code/faasm/wasm/sgd
+./bin/codegen /usr/local/code/faasm/wasm/demo
+./bin/codegen /usr/local/code/faasm/wasm/errors
+./bin/codegen /usr/local/code/faasm/wasm/python
+./bin/codegen /usr/local/code/faasm/wasm/sgd
 
 echo "Build tests"
-cmake --build . --target tests  -- -j
+cmake --build . --target tests
 
 popd
