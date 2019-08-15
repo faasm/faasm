b54f87c7d2124c7ba223a5a514979040d6652f41
Simon Shillaker <shillaker@gmail.com>
2019-08-15 11:39:39 +0100

    Reverted to config map

--- bin/cgroup.sh
+++ bin/cgroup.sh
@@ -7,6 +7,12 @@ CGROUP_USER=${CGROUP_USER:-$(whoami)}
 
 CGROUP=cpu:faasm
 
+if [ "$CGROUP_MODE" == "off" ];
+then
+    echo "CGroup support is off"
+    exit 0
+fi
+
 echo "Setting up cgroup ${CGROUP} for ${CGROUP_USER}"
 
 cgcreate -t ${CGROUP_USER}:${CGROUP_USER} -a ${CGROUP_USER}:${CGROUP_USER} -g ${CGROUP}
