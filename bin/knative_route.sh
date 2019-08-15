fa90d2199a19e92e50127c70eea8839bd54d5659
Simon Shillaker <shillaker@gmail.com>
2019-08-15 11:51:17 +0100

    Fixed knative route script

--- bin/knative_route.sh
+++ bin/knative_route.sh
@@ -12,7 +12,7 @@ kubectl get svc istio-ingressgateway --namespace istio-system
 # Print details of the function route
 echo ""
 echo "----- Function Route -----"
-kubectl get route ${FUNC_NAME}
+kubectl --namespace faasm get route ${FUNC_NAME}
 
 # Get IP and host for this service
 IP_ADDRESS=$(kubectl get node  --output 'jsonpath={.items[0].status.addresses[0].address}')
@@ -20,7 +20,7 @@ IP_ADDRESS=$(kubectl get node  --output 'jsonpath={.items[0].status.addresses[0]
 PORT=$(kubectl get svc istio-ingressgateway --namespace istio-system   --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')
 
 HOST=${IP_ADDRESS}:${PORT}
-FUNC_HOST=${FUNC_NAME}.default.example.com
+FUNC_HOST=${FUNC_NAME}.faasm.example.com
 echo ""
 echo "----- Request details -----"
 echo "HOST = ${HOST}"
