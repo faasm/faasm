21a4c4e383a81a661b4660f0e7f48e58a9bc350e
PhD student LSDS <scs17@wombat34.doc.res.ic.ac.uk>
2019-08-15 12:14:17 +0000

    KNative routes

--- bin/knative_route.sh
+++ bin/knative_route.sh
@@ -1,36 +1,47 @@
 #!/bin/bash
 
 FUNC_NAME=faasm-test
+FUNC_HOST=${FUNC_NAME}.faasm.example.com
 
 set -e
 
+echo "--------------------------------"
+echo "-----    Top-level info    -----"
+echo "--------------------------------"
+
 # Print details of the top-level gateway
 echo ""
-echo "----- Istio Gateway -----"
+echo "-----     Istio Gateway    -----"
+echo ""
 kubectl get svc istio-ingressgateway --namespace istio-system
 
 # Print details of the function route
 echo ""
-echo "----- Function Route -----"
+echo "-----    Function Route    -----"
+echo ""
 kubectl --namespace faasm get route ${FUNC_NAME}
 
-# Get IP and host for this service
-IP_ADDRESS=$(kubectl get node  --output 'jsonpath={.items[0].status.addresses[0].address}')
+# Get IP and host for the K8s master node
+MASTER_IP=$(kubectl get node  --output 'jsonpath={.items[0].status.addresses[0].address}')
 
-PORT=$(kubectl get svc istio-ingressgateway --namespace istio-system   --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')
+MASTER_PORT=$(kubectl get svc istio-ingressgateway --namespace istio-system   --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')
 
-HOST=${IP_ADDRESS}:${PORT}
-FUNC_HOST=${FUNC_NAME}.faasm.example.com
-echo ""
-echo "----- Request details -----"
-echo "HOST = ${HOST}"
-echo "FUNCTION HOST = ${FUNC_HOST}"
+MASTER_HOST=${MASTER_IP}:${MASTER_PORT}
 
 echo ""
-echo "----- Curl -----"
-echo "CMD: "
-echo "curl -H 'Host: ${FUNC_HOST}' http://${HOST}"
+echo "--------------------------------"
+echo "----- Function Invocation  -----"
+echo "--------------------------------"
+echo ""
+echo "curl -H 'Host: ${FUNC_HOST}' http://${MASTER_HOST}"
 
 echo ""
-echo "----- Upload -----"
-kubectl get service upload --namespace=faasm
+echo "--------------------------------"
+echo "-----        Upload        -----"
+echo "--------------------------------"
+echo ""
+UPLOAD_IP=$(kubectl get --namespace=faasm service upload  --output 'jsonpath={.spec.clusterIP}')
+UPLOAD_PORT=8002
+
+echo "curl -X PUT http://${UPLOAD_IP}:${UPLOAD_PORT}/f/<user>/<func> -T <wasm_file>"
+echo ""
