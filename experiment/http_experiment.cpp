74eb4ff7ac457a12299ff2a39184c5234095c344
Simon Shillaker <mail@simonshillaker.com>
2019-08-20 10:12:45 +0100

    more fiddling

--- experiment/http_experiment.cpp
+++ experiment/http_experiment.cpp
@@ -1,4 +1,6 @@
 #include "pistache/endpoint.h"
+#include <util/logging.h>
+#include <signal.h>
 
 using namespace Pistache;
 
@@ -12,6 +14,19 @@ public:
 };
 
 int main() {
+    const std::shared_ptr<spdlog::logger> &logger = util::getLogger();
+
+    // Set up signal handler
+    sigset_t signals;
+    if (sigemptyset(&signals) != 0
+        || sigaddset(&signals, SIGTERM) != 0
+        || sigaddset(&signals, SIGINT) != 0
+        || sigaddset(&signals, SIGHUP) != 0
+        || pthread_sigmask(SIG_BLOCK, &signals, nullptr) != 0) {
+
+        throw std::runtime_error("Install signal handler failed");
+    }
+
     Pistache::Address addr(Pistache::Ipv4::any(), Pistache::Port(8080));
     auto opts = Pistache::Http::Endpoint::options()
             .threads(5);
@@ -20,4 +35,13 @@ int main() {
     server.init(opts);
     server.setHandler(Http::make_handler<HelloHandler>());
     server.serve();
+
+    // Wait for a signal
+    int signal = 0;
+    int status = sigwait(&signals, &signal);
+    if (status == 0) {
+        logger->info("Received signal: {}", signal);
+    } else {
+        logger->info("Sigwait return value: {}", signal);
+    }
 }
