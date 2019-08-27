a0d1e5de7d292e7ee8e509852574d90b69bd586f
Simon Shillaker <mail@simonshillaker.com>
2019-08-27 10:17:51 +0100

    Added faasm tpt script

---
+++ src/runner/bench_tpt.cpp
@@ -0,0 +1,92 @@
+#include <wasm/WasmModule.h>
+#include <util/config.h>
+#include <util/timing.h>
+#include <util/locks.h>
+#include <runner/function.h>
+#include <stdio.h>
+#include <fstream>
+
+#define USER "demo"
+#define FUNCTION "noop"
+
+#define TPT_LOG_FILE "/tmp/faasm_tpt.log"
+#define LAT_LOG_FILE "/tmp/faasm_lat.log"
+
+static std::mutex latFileMx;
+
+void _execFunction() {
+    const util::TimePoint &start = util::startTimer();
+
+    runner::benchmarkExecutor(USER, FUNCTION);
+
+    double elapsedMillis = util::getTimeDiffMillis(start);
+
+    std::ofstream latFile;
+    latFile.open(LAT_LOG_FILE, std::ios_base::app);
+
+    // Write to latency file with lock
+    util::UniqueLock lock(latFileMx);
+    latFile << elapsedMillis << " LATENCY" << std::endl;
+}
+
+
+int main(int argc, char *argv[]) {
+    util::initLogging();
+    const std::shared_ptr<spdlog::logger> logger = util::getLogger();
+
+    if (argc < 3) {
+        logger->error("Must provide request delay (ms) and duration (ms)");
+        return 1;
+    }
+
+    // Get args
+    int requestDelay = std::stoi(argv[1]);
+    int duration = std::stoi(argv[2]);
+
+    logger->info("Running Faasm throughput bench with delay={}ms and duration={}ms", requestDelay, duration);
+
+    // Set up conf
+    util::SystemConfig &conf = util::getSystemConfig();
+    conf.cgroupMode = "on";
+    conf.netNsMode = "off";
+    conf.zygoteMode = "off";
+
+    // Set up files
+    remove(LAT_LOG_FILE);
+    remove(TPT_LOG_FILE);
+
+    std::ofstream tptFile;
+    tptFile.open(TPT_LOG_FILE, std::ios_base::app);
+
+    const util::TimePoint &startTimer = util::startTimer();
+    double elapsed = 0;
+    int requestCount = 0;
+
+    std::vector<std::thread> threads;
+
+    while (elapsed < duration) {
+        // Spawn execution in background
+        threads.emplace_back(_execFunction);
+
+        // Log to throughput file
+        tptFile << elapsed << " REQUEST " << requestCount << std::endl;
+
+        // Sleep
+        usleep(requestDelay * 1000);
+
+        // Update elapsed
+        elapsed = util::getTimeDiffMillis(startTimer);
+        std::cout << "ELAPSED = " << elapsed << std::endl;
+    }
+
+    // Wait for any stragglers
+    for (auto &t : threads) {
+        if (t.joinable()) {
+            t.join();
+        }
+    }
+
+    std::cout << "Finished";
+
+    return 0;
+}
