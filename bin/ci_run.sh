7984a1a7482f36d35b9fa7c91620c345fc605bee
Simon Shillaker <mail@simonshillaker.com>
2019-09-26 18:42:44 +0100

    More codegen stuff

--- bin/ci_run.sh
+++ bin/ci_run.sh
@@ -9,13 +9,14 @@ inv compile-libfake --clean
 popd
 
 echo "Run codegen on wasm files"
-/faasm/build/bin/codegen /usr/local/code/faasm/wasm/demo
-/faasm/build/bin/codegen /usr/local/code/faasm/wasm/errors
-/faasm/build/bin/codegen /usr/local/code/faasm/wasm/python
-/faasm/build/bin/codegen /usr/local/code/faasm/wasm/sgd
+/faasm/build/bin/codegen_func demo
+/faasm/build/bin/codegen_func errors
+/faasm/build/bin/codegen_func python
+/faasm/build/bin/codegen_func sgd
+/faasm/build/bin/codegen_func tf
 
 echo "Run codegen on python files"
-/faasm/build/bin/codegen /usr/local/faasm/runtime_root/lib/python3.7
+/faasm/build/bin/codegen_shared_obj /usr/local/faasm/runtime_root/lib/python3.7
 
 echo "Execute tests"
 /faasm/build/bin/tests
--- docker/testing.dockerfile
+++ docker/testing.dockerfile
@@ -38,9 +38,11 @@ RUN inv restore-emsdk
 # Fix ownership of runtime root
 RUN chown -R root:root /usr/local/faasm
 
-# Build the tests
+# Build the tests and codegen
 WORKDIR /faasm/build
 RUN cmake --build . --target tests -- -j
+RUN cmake --build . --target codegen_shared_obj -- -j
+RUN cmake --build . --target codegen_func -- -j
 
 # Command to run the tests
 CMD /faasm/build/bin/tests
