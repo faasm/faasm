eba978ca4cd9d8d3182f8a57365bae338b33a309
Simon Shillaker <s.shillaker17@imperial.ac.uk>
2019-10-15 16:16:23 +0000

    Updated knative route script

--- bin/knative_route.sh
+++ bin/knative_route.sh
@@ -5,19 +5,18 @@ FUNC_HEADER=faasm-worker.faasm.example.com
 set -e
 
 # Get invoke details
-MASTER_IP=$(kubectl get node --output 'jsonpath={.items[0].status.addresses[0].address}')
-MASTER_PORT=$(kubectl get svc istio-ingressgateway --namespace istio-system   --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')
-MASTER_HOST=${MASTER_IP}:${MASTER_PORT}
+ISTIO_IP=$(kubectl get svc istio-ingressgateway --namespace istio-system --output 'jsonpath={.spec.clusterIP}')
+#ISTIO_PORT=$(kubectl get svc istio-ingressgateway --namespace istio-system   --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')
+ISTIO_PORT=80
 
 # Get upload details
 UPLOAD_IP=$(kubectl get --namespace=faasm service upload  --output 'jsonpath={.spec.clusterIP}')
-UPLOAD_EXTERNAL_IP=$(kubectl get --namespace=faasm service upload  --output 'jsonpath={.spec.externalIPs[0]}')
 UPLOAD_PORT=8002
 
 echo ""
 echo "-----        Invoke        -----"
 echo ""
-echo "curl -H 'Host: ${FUNC_HEADER}' http://${MASTER_HOST}"
+echo "curl -H 'Host: ${FUNC_HEADER}' http://${ISTIO_IP}"
 
 echo ""
 echo "-----        Upload        -----"
@@ -28,8 +27,9 @@ echo ""
 echo "-----       INI file       -----"
 echo ""
 echo "[Kubernetes]"
-echo "invoke_host = ${MASTER_IP}"
-echo "invoke_port = ${MASTER_PORT}"
+echo "invoke_host = ${ISTIO_IP}"
+echo "invoke_port = ${ISTIO_PORT}"
 echo "upload_host = ${UPLOAD_IP}"
 echo "upload_port = ${UPLOAD_PORT}"
 echo ""
+
