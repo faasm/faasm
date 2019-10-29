e8990d3f2566579118df8aa454eae53103092068
Simon Shillaker <mail@simonshillaker.com>
2019-10-29 18:41:11 +0000

    Pyfaasm

--- func/python/faasm_test.py
+++ func/python/faasm_test.py
@@ -1,8 +1,3 @@
-import pyfaasm
+from pyfaasm.core import checkPythonBindings
 
-output = pyfaasm.hello_faasm()
-print(output)
-
-faasm_in = pyfaasm.faasm_get_input()
-print(faasm_in)
-
+checkPythonBindings()
