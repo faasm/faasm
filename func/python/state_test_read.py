9f50c178651f76c285ff36aeaa137ba34deb856e
Simon Shillaker <mail@simonshillaker.com>
2019-10-30 14:03:07 +0000

    e2e python state

--- func/python/state_test_read.py
+++ func/python/state_test_read.py
@@ -1,7 +1,27 @@
-from pyfaasm.core import getInput, setOutput
+from pyfaasm.core import getState, getStateOffset
 
-i = getInput()
+key = "pyStateTest"
+valueLen = 10
+expected = b'0199956789'
 
-print("Got input {}".format(i))
+# Check the full value
+actual = getState(key, valueLen)
+if actual != expected:
+    msg = "Mismatch: actual {}, expected {})".format(actual, expected)
+    print(msg)
+    raise RuntimeError(msg)
 
-setOutput(bytes(i))
+# Check a couple of segments
+actualSegmentA = getStateOffset(key, valueLen, 0, 3)
+expectedA = b'019'
+if actualSegmentA != expectedA:
+    msg = "Mismatched segment: actual {}, expected {})".format(actualSegmentA, expectedA)
+    print(msg)
+    raise RuntimeError(msg)
+
+actualSegmentB = getStateOffset(key, valueLen, 4, 4)
+expectedB = b'9567'
+if actualSegmentB != expectedB:
+    msg = "Mismatched segment: actual {}, expected {})".format(actualSegmentB, expectedB)
+    print(msg)
+    raise RuntimeError(msg)
--- func/python/state_test_write.py
+++ func/python/state_test_write.py
@@ -1,6 +1,5 @@
-from pyfaasm.core import getState, getStateOffset, setState, setStateOffset
+from pyfaasm.core import getState, getStateOffset, setState, setStateOffset, pushState, pullState
 
-# Set the full value
 key = "pyStateTest"
 valueLen = 10
 fullValue = b'0123456789'
@@ -10,27 +9,33 @@ offset = 2
 segmentLen = 3
 modifiedValue = b'0199956789'
 
+
 def _check_full_value(expected):
+    pullState(key, valueLen)
     actual = getState(key, valueLen)
     if actual != expected:
         msg = "Mismatch: actual {}, expected {})".format(actual, expected)
         print(msg)
         raise RuntimeError(msg)
 
+
 # Set the full value initially
-setState(key, fullValue, valueLen)
+setState(key, fullValue)
+pushState(key)
 
 # Check the full value has been written
 _check_full_value(fullValue)
 
 # Update a segment
-setStateOffset(key, valueLen, offset, segment, segmentLen)
+setStateOffset(key, valueLen, offset, segment)
+pushState(key)
 
 # Check the full value again
-actualValue = getState(key, valueLen)
 _check_full_value(modifiedValue)
 
 # Check just the segment
 actualSegment = getStateOffset(key, valueLen, offset, segmentLen)
 if actualSegment != segment:
-    
+    msg = "Mismatched segment: actual {}, expected {})".format(actualSegment, segment)
+    print(msg)
+    raise RuntimeError(msg)
