71cd4a773a36da10a72c2797cc7c2303d423de24
Simon Shillaker <mail@simonshillaker.com>
2019-11-20 12:19:45 +0000

    Changing matrix dimensions

--- tasks/matrix_data.py
+++ tasks/matrix_data.py
@@ -11,8 +11,8 @@ from tasks.util.matrices import get_matrix_dir, MATRIX_CONF_STATE_KEY, SUBMATRIC
 
 @task
 def generate_all_matrix_data(ctx):
-    splits = [3, 4]
-    sizes = [1000, 2000, 3000, 4000, 5000, 6000]
+    splits = [2]
+    sizes = [128, 512, 1024, 2048, 4096, 8192]
     for n_splits in splits:
         for matrix_size in sizes:
             generate_matrix_data(ctx, matrix_size, n_splits)
