7df1f8cc5ddc54bf2e1a08144fa9f8bc0f69415a
Simon Shillaker <554768+Shillaker@users.noreply.github.com>
2019-11-28 20:02:59 +0000

    Added back munmap (#111)

--- func/demo/mmap_big.cpp
+++ func/demo/mmap_big.cpp
@@ -7,16 +7,28 @@
 #include <errno.h>
 
 FAASM_MAIN_FUNC() {
-    // Make sure we can allocate more than 2GB of memory (i.e. >32768 pages)
-    size_t nPages = 35000;
+    // Make sure we can allocate more than 1GB of memory (i.e. >16384 pages)
+    size_t nPages = 18000;
     size_t chunkSize = 1000;
     size_t nChunks = nPages / chunkSize;
+    size_t chunkLen = (chunkSize * 64 * 1024);
 
+    void **regions = new void*[nChunks];
     for(int i = 0; i < nChunks; i++) {
-        size_t memLen = (chunkSize * 64 * 1024);
-        void *p = mmap(nullptr, memLen, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANON | MAP_PRIVATE, -1, 0);
+        void *p = mmap(nullptr, chunkLen, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANON | MAP_PRIVATE, -1, 0);
         if (p == MAP_FAILED) {
             printf("ERROR - mmap failed: %s\n", strerror(errno));
+            return 1;
+        }
+
+        regions[i] = p;
+    }
+
+    for(int i = nChunks - 1; i >= 0; i--) {
+        void *p = regions[i];
+        int res = munmap(p, chunkLen);
+        if(res != 0) {
+            printf("ERROR - munmap failed: %s\n", strerror(errno));
             return 1;
         }
     }
