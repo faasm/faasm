b4695cc6b24b9c104168815278c1f09f5fdedbae
Simon Shillaker <mail@simonshillaker.com>
2020-01-06 11:59:26 +0000

    Pyperf

--- func/python/bench_pyflate.py
+++ func/python/bench_pyflate.py
@@ -5,7 +5,7 @@ from pyperformance.benchmarks.bm_pyflate import bench_pyflake
 
 if __name__ == "__main__":
     if os.environ.get("PYTHONWASM") == "1":
-        file_path = "/lib/python3.7/site-packages/pyperformance/benchmarks/data/telco-bench.b"
+        file_path = "/lib/python3.7/site-packages/pyperformance/benchmarks/data/interpreter.tar.bz2"
     else:
         file_path = "/usr/local/code/faasm/venv/lib/python3.6/site-packages/pyperformance/benchmarks/data/interpreter.tar.bz2"
         if not exists(file_path):
