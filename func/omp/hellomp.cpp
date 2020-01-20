29d655de20fa70cbf0fe95cb863189ebeab6f73c
Simon Shillaker <mail@simonshillaker.com>
2020-01-20 17:44:08 +0000

    Faasm annotation

--- func/omp/hellomp.cpp
+++ func/omp/hellomp.cpp
@@ -1,10 +1,14 @@
 #include <omp.h>
 #include <stdio.h>
+#include <faasm/faasm.h>
 
-int main() {
+FAASM_MAIN_FUNC() {
+    printf("Starting OpenMP\n");
 #pragma omp parallel default(none)
     {
         int id = omp_get_thread_num();
         printf("OMP thread %d\n", id);
     };
+
+    return 0;
 }
