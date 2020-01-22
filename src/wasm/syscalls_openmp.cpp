31fd797be4b2e681e4a1f702789c237358e69b92
Simon Shillaker <scs17@ic.ac.uk>
2020-01-22 17:49:02 +0000

    Adding to fork call

--- src/wasm/syscalls_openmp.cpp
+++ src/wasm/syscalls_openmp.cpp
@@ -1,5 +1,6 @@
 #include "WasmModule.h"
 #include "syscalls.h"
+#include "openmp_types.h"
 
 #include <WAVM/Runtime/Runtime.h>
 #include <WAVM/Runtime/Intrinsics.h>
@@ -11,8 +12,32 @@ namespace wasm {
         return 1;
     }
 
-    WAVM_DEFINE_INTRINSIC_FUNCTION(env, "__kmpc_fork_call", void, __kmpc_fork_call, I32 a, I32 b, I32 c, I32 d) {
-        util::getLogger()->debug("S - __kmpc_fork_call {} {} {} {}", a, b, c, d);
+    /**
+     * Function defined in openmp/runtime/src/kmp_csupport.cpp
+     */
+    WAVM_DEFINE_INTRINSIC_FUNCTION(env, "__kmpc_fork_call", void, __kmpc_fork_call, I32 locPtr, I32 argc,
+                                   I32 microtaskPtr, I32 argsPtr) {
+        // Extract the source location from the wasm memory
+        Runtime::Memory *memoryPtr = getExecutingModule()->defaultMemory;
+        wasm_ident *wasmLoc = &Runtime::memoryRef<wasm_ident>(memoryPtr, locPtr);
+
+        const std::shared_ptr<spdlog::logger> &logger = util::getLogger();
+        logger->debug("S - __kmpc_fork_call {} {} {} {}", locPtr, argc, microtaskPtr, argsPtr);
+        logger->debug("S - __kmpc_fork_call wasmLoc.psource = {}", wasmLoc->psource);
+
+        // Note that microtaskPtr here is a function pointer and index in the wasm function table
+        logger->debug("S - __kmpc_fork_call microtaskPtr = {}", microtaskPtr);
+
+        // Retrieve the function from the table
+        Runtime::Object *funcObj = Runtime::getTableElement(getExecutingModule()->defaultTable, microtaskPtr);
+        Runtime::Function *func = Runtime::asFunction(funcObj);
+
+        // Here we want to execute functions on the module as we would normally but in a new context
+        auto threadContext = createContext(getCompartmentFromContextRuntimeData(contextRuntimeData));
+        IR::FunctionType funcType = Runtime::getFunctionType(func);
+        IR::UntaggedValue result;
+        std::vector<IR::UntaggedValue> arguments;
+        Runtime::invokeFunction(threadContext, func, funcType, arguments.data(), &result);
     }
 
     void ompLink() {
