ce51b7d84d729c4591fbbae7e3e90de2abdc8b31
Simon Shillaker <mail@simonshillaker.com>
2020-01-23 09:38:53 +0000

    Slight rearrange

--- src/wasm/syscalls_openmp.cpp
+++ src/wasm/syscalls_openmp.cpp
@@ -15,61 +15,60 @@ namespace wasm {
     }
 
     /**
-     * Function defined in openmp/runtime/src/kmp_csupport.cpp
-     *
+     * "Real" Function defined in openmp/runtime/src/kmp_csupport.cpp
      * Actual implementation calls __kmp_fork_call under the hood (openmp/runtime/src/kmp_runtime.cpp)
      *
-     * Note that microtaskPtr is a function pointer. The type of this function pointer is defined as
-     * microtask_t in kmp_csupport.
+     * Note that microtaskPtr is a function pointer. The type of this function pointer is a microtask_t
+     * which is defined in kmp_csupport.cpp.
      */
     WAVM_DEFINE_INTRINSIC_FUNCTION(env, "__kmpc_fork_call", void, __kmpc_fork_call, I32 locPtr, I32 argc,
                                    I32 microtaskPtr, I32 argsPtr) {
-        // Extract the source location from the wasm memory
-        Runtime::Memory *memoryPtr = getExecutingModule()->defaultMemory;
-        wasm_ident *wasmLoc = &Runtime::memoryRef<wasm_ident>(memoryPtr, locPtr);
+        // Looks like we don't need the source loc info (accessing it commented out below though)
+        // Runtime::Memory *memoryPtr = getExecutingModule()->defaultMemory;
+        // wasm_ident *wasmLoc = &Runtime::memoryRef<wasm_ident>(memoryPtr, locPtr);
+        // logger->debug("wasmLoc.psource = {}", wasmLoc->psource);
 
         const std::shared_ptr<spdlog::logger> &logger = util::getLogger();
         logger->debug("S - __kmpc_fork_call {} {} {} {}", locPtr, argc, microtaskPtr, argsPtr);
-        logger->debug("S - __kmpc_fork_call wasmLoc.psource = {}", wasmLoc->psource);
 
-        // Note that microtaskPtr here is a function pointer and index in the wasm function table
-        logger->debug("S - __kmpc_fork_call microtaskPtr = {}", microtaskPtr);
+        unsigned int nCores = util::getUsableCores();
 
         // Retrieve the function from the table
         Runtime::Object *funcObj = Runtime::getTableElement(getExecutingModule()->defaultTable, microtaskPtr);
         Runtime::Function *func = Runtime::asFunction(funcObj);
         IR::FunctionType funcType = Runtime::getFunctionType(func);
 
-        unsigned int nCores = util::getUsableCores();
-
         // Spawn function calls in threads in a loop
         std::vector<std::thread> threads;
         for (int threadNum = 0; threadNum < nCores; threadNum++) {
             WasmModule *parentModule = getExecutingModule();
             message::Message *parentCall = getExecutingCall();
-            threads.emplace_back([threadNum, argc, argsPtr, func, funcType, contextRuntimeData, parentModule, parentCall] {
-                // Assign this thread its relevant thread number
-                thisThreadNumber = threadNum;
 
+            // TODO - check this is correct
+            // If we have no arguments to pass to the nested function, it only expects 2.
+            // If we have more, we need to say how many and pass a pointer to them.
+            std::vector<IR::UntaggedValue> arguments;
+            if(argc > 0) {
+                arguments = {thisThreadNumber, argc, argsPtr};
+            } else {
+                arguments = {thisThreadNumber, 0};
+            }
+
+            threads.emplace_back([threadNum, arguments, func, funcType, contextRuntimeData, parentModule, parentCall] {
                 // Note that executing module and call are stored in TLS so need to explicitly set here
                 setExecutingModule(parentModule);
                 setExecutingCall(parentCall);
 
+                // Assign this thread its relevant thread number
+                thisThreadNumber = threadNum;
+
                 // Create a new context for this thread
                 auto threadContext = createContext(getCompartmentFromContextRuntimeData(contextRuntimeData));
 
-                // TODO - check this is correct
-                // If we have no arguments to pass to the nested function, it only expects 2.
-                // If we have more, we need to say how many and pass a pointer to them.
-                std::vector<IR::UntaggedValue> arguments;
-                if(argc > 0) {
-                    arguments = {thisThreadNumber, argc, argsPtr};
-                } else {
-                    arguments = {thisThreadNumber, 0};
-                }
-
                 IR::UntaggedValue result;
                 Runtime::invokeFunction(threadContext, func, funcType, arguments.data(), &result);
+
+                // TODO - check the result here
             });
         }
 
