4a8fe23b143918c93c9eb70f748e47c0bef9be4a
Simon Shillaker <mail@simonshillaker.com>
2020-01-27 11:48:58 +0000

    Non-global shared data

--- func/omp/hellomp.cpp
+++ func/omp/hellomp.cpp
@@ -2,21 +2,55 @@
 #include <stdio.h>
 #include <faasm/faasm.h>
 
-void printThreadInfo(const char* label) {
-    int thisThread = omp_get_thread_num();
-    unsigned int totalThreads = omp_get_num_threads();
-    printf("%s %d of %d\n", label, thisThread, totalThreads);
-}
+bool failed = false;
+
+/**
+ * Note - this function is designed to test basic OpenMP functionality.
+ * The loop must include both globally and locally shared variables.
+ */
 
 FAASM_MAIN_FUNC() {
-    printThreadInfo("Main before");
-
-    // OMP parallel section
-    #pragma omp parallel default(none)
-    {
-        printThreadInfo("OMP thread");
+    int nThreads = omp_get_max_threads();
+    if(nThreads < 2) {
+        printf("Need 2 or more threads but got %i\n", nThreads);
+        return 1;
     }
 
-    printThreadInfo("Main after");
+    int mainThreadNum = omp_get_thread_num();
+    if(mainThreadNum != 0) {
+        printf("Main thread is not zero\n");
+        return 1;
+    }
+
+    bool *flags = new bool[nThreads];
+
+    // Note the shared variables here are both local and global
+    #pragma omp parallel shared(flags, failed, nThreads)
+    {
+        int thisThreadNum = omp_get_thread_num();
+        flags[thisThreadNum] = true;
+
+        unsigned int currentThreads = omp_get_num_threads();
+        if(currentThreads != nThreads) {
+            printf("Only running %i threads but expected %i\n", currentThreads, nThreads);
+            failed = true;
+        }
+    }
+
+    if(failed) {
+        printf("Did not get expected number of threads in parallel section\n");
+        return 1;
+    }
+
+    for(int i = 0; i < nThreads; i++) {
+        if(!flags[i]) {
+            printf("Did not get a true flag for thread %i\n", i);
+            return 1;
+        }
+    }
+
+    printf("OpenMP checks succeeded\n");
+
+    delete[] flags;
     return 0;
 }
