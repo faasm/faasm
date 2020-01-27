70dbbd5c58154c5d33a8e736b00b6a45488a8487
Simon Shillaker <mail@simonshillaker.com>
2020-01-27 17:55:13 +0000

    Tidy up

--- bin/entrypoint_codegen.sh
+++ bin/entrypoint_codegen.sh
@@ -20,7 +20,7 @@ if [[ "${TF_CODEGEN}" == "on" ]]; then
 fi
 
 if [[ "${PYTHON_CODEGEN}" == "on" ]]; then
-  if [[ ! -f "$OBJ_DIR/python/py_func/function.wasm.o"]]; then
+  if [[ ! -f "$OBJ_DIR/python/py_func/function.wasm.o" ]]; then
     echo "Generating python object files"
     ./bin/codegen_func python py_func
     ./bin/codegen_shared_obj /usr/local/faasm/runtime_root/lib/python3.7
