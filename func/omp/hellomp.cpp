ebb016866ac3c7b740680df29af341298bc5ee18
Simon Shillaker <mail@simonshillaker.com>
2020-01-27 12:12:59 +0000

    Reverted hellomp

--- func/omp/hellomp.cpp
+++ func/omp/hellomp.cpp
@@ -2,55 +2,20 @@
 #include <stdio.h>
 #include <faasm/faasm.h>
 
-bool failed = false;
-
-/**
- * Note - this function is designed to test basic OpenMP functionality.
- * The loop must include both globally and locally shared variables.
- */
+void printThreadInfo(const char* label) {
+    int thisThread = omp_get_thread_num();
+    unsigned int totalThreads = omp_get_num_threads();
+    printf("%s %d of %d\n", label, thisThread, totalThreads);
+}
 
 FAASM_MAIN_FUNC() {
-    int nThreads = omp_get_max_threads();
-    if(nThreads < 2) {
-        printf("Need 2 or more threads but got %i\n", nThreads);
-        return 1;
-    }
+    printThreadInfo("Main before");
 
-    int mainThreadNum = omp_get_thread_num();
-    if(mainThreadNum != 0) {
-        printf("Main thread is not zero\n");
-        return 1;
-    }
-
-    bool *flags = new bool[nThreads];
-
-    // Note the shared variables here are both local and global
-    #pragma omp parallel shared(flags, failed, nThreads)
+    #pragma omp parallel default(none)
     {
-        int thisThreadNum = omp_get_thread_num();
-        flags[thisThreadNum] = true;
-
-        unsigned int currentThreads = omp_get_num_threads();
-        if(currentThreads != nThreads) {
-            printf("Only running %i threads but expected %i\n", currentThreads, nThreads);
-            failed = true;
-        }
+        printThreadInfo("OMP thread");
     }
 
-    if(failed) {
-        printf("Did not get expected number of threads in parallel section\n");
-        return 1;
-    }
-
-    for(int i = 0; i < nThreads; i++) {
-        if(!flags[i]) {
-            printf("Did not get a true flag for thread %i\n", i);
-            return 1;
-        }
-    }
-
-    printf("OpenMP checks succeeded\n");
-
-    delete[] flags;
+    printThreadInfo("Main after");
     return 0;
 }
