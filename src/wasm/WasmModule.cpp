4b503f10fefa8cf01f011890abc81b4c6ad7ae8a
Simon Shillaker <mail@simonshillaker.com>
2020-01-31 13:32:59 +0000

    Formatting

--- src/wasm/WasmModule.cpp
+++ src/wasm/WasmModule.cpp
@@ -339,7 +339,7 @@ namespace wasm {
         defaultTable = Runtime::getDefaultTable(moduleInstance);
 
         // Get and execute zygote function
-        if(executeZygote) {
+        if (executeZygote) {
             zygoteFunctionInstance = getFunction(ZYGOTE_FUNC_NAME, false);
             if (zygoteFunctionInstance) {
                 IR::UntaggedValue result;
@@ -365,7 +365,7 @@ namespace wasm {
             I32 heapBase = getGlobalI32("__heap_base", executionContext);
             I32 dataEnd = getGlobalI32("__data_end", executionContext);
 
-            if(heapBase >0 && dataEnd > 0 && heapBase != dataEnd) {
+            if (heapBase > 0 && dataEnd > 0 && heapBase != dataEnd) {
                 logger->error("Appears stack is not at bottom (__heap_base={} __data_end={})", heapBase, dataEnd);
                 throw std::runtime_error("Wasm memory layout not as expected");
             }
@@ -516,7 +516,7 @@ namespace wasm {
     I32 WasmModule::getGlobalI32(const std::string &globalName, Runtime::Context *context) {
         Runtime::Global *globalPtr = Runtime::asGlobal(Runtime::getInstanceExport(moduleInstance, globalName.c_str()));
 
-        if(globalPtr == nullptr) {
+        if (globalPtr == nullptr) {
             return -1;
         }
 
@@ -1093,7 +1093,7 @@ namespace wasm {
         // Restore memory
         Uptr currentNumPages = Runtime::getMemoryNumPages(defaultMemory);
         size_t pagesRequired = mem.numPages - currentNumPages;
-        if(pagesRequired > 0) {
+        if (pagesRequired > 0) {
             mmapPages(pagesRequired);
         }
 
