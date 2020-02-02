93770c1ef5f068a660b4db8f41b0ee952fa29dca
Mayeul Fournial <mfournial@outlook.com>
2020-02-02 22:22:48 +0000

    Fix codestyle of AlignedElem in omp_helper

--- func/omp/omp_helper.h
+++ func/omp/omp_helper.h
@@ -2,9 +2,8 @@
 #define FAASM_OMP_HELPER_H
 
 struct AlignedElem {
-    int32_t i;
-    AlignedElem() : i{0} {};
-    AlignedElem(int32_t i) : i{i} {};
+    int32_t i = 0;
+    AlignedElem(int32_t i) : i(i) {};
     friend inline bool operator==(const AlignedElem& lhs, const AlignedElem& rhs) {
         return lhs.i == rhs.i;
     };
