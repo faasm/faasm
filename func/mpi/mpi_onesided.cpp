38d6344954f634efaff56d71c663037bf11257fa
Simon Shillaker <mail@simonshillaker.com>
2020-02-18 13:03:03 +0000

    One-sided example native

--- func/mpi/mpi_onesided.cpp
+++ func/mpi/mpi_onesided.cpp
@@ -19,12 +19,6 @@ FAASM_MAIN_FUNC() {
     MPI_Get_processor_name(name, &len);
     printf("Rank %d running on %s\n", rank, name);
 
-    // Drop out if not in ranks 0-2
-    if (rank < 3) {
-        MPI_Finalize();
-        return 0;
-    }
-
     // Hard-code the other ranks this one will talk to and
     // receive data from
     int getRank;
@@ -45,58 +39,58 @@ FAASM_MAIN_FUNC() {
     }
 
     // Generate some data
-    int rankData = MPI_Alloc_mem();
-    int expectedGetData[NUM_ELEMENT];
-    int expectedPutData[NUM_ELEMENT];
+    int dataSize;
+    MPI_Type_size(MPI_INT, &dataSize);
+    MPI_Aint memSize = NUM_ELEMENT * dataSize;
+    int *sharedData;
+    MPI_Alloc_mem(memSize, MPI_INFO_NULL, &sharedData);
+
     int putData[NUM_ELEMENT];
 
-    for (int i = 0; i < NUM_ELEMENT; i++) {
-        // Data to store in this rank
-        rankData[i] = 10 * rank + i;
+    int expectedGetData[NUM_ELEMENT];
+    int expectedPutData[NUM_ELEMENT];
 
-        // Copy of the rank data
+    for (int i = 0; i < NUM_ELEMENT; i++) {
+        // Populate the existing shared mem along with a copy
+        sharedData[i] = 10 * rank + i;
         putData[i] = 10 * rank + i;
 
-        // Data we expect to get/ have put
+        // Data we expect to get from one rank and have put into our shared mem by another
         expectedGetData[i] = 10 * getRank + i;
         expectedPutData[i] = 10 * putterRank + i;
     }
 
-    // Get size of data
-    int dataSize;
-    MPI_Type_size(MPI_INT, &dataSize);
-
-    // Create a window and write data to it
+    // Create a window on the shared data
     MPI_Win window;
-    MPI_Win_create(rankData, NUM_ELEMENT * dataSize, dataSize, MPI_INFO_NULL, MPI_COMM_WORLD, &window);
+    MPI_Win_create(sharedData, NUM_ELEMENT * dataSize, dataSize, MPI_INFO_NULL, MPI_COMM_WORLD, &window);
 
-    // Start epoch
-    MPI_Win_fence(0, window);
-
-    // Get values from another rank
+    // Get data from another rank's shared memory
     int actual[NUM_ELEMENT];
-    MPI_Get(actual, NUM_ELEMENT, MPI_INT, getRank, 0, NUM_ELEMENT, MPI_INT, window);
-
-    // End epoch
+    MPI_Win_fence(0, window);
+    if(rank < 3) {
+        MPI_Get(actual, NUM_ELEMENT, MPI_INT, getRank, 0, NUM_ELEMENT, MPI_INT, window);
+    }
     MPI_Win_fence(0, window);
 
     // Check we've got the data we expected
-    if (!faasm::compareIntArrays(actual, expectedGetData, NUM_ELEMENT)) {
+    if(rank < 3 && !faasm::compareIntArrays(actual, expectedGetData, NUM_ELEMENT)) {
         return 1;
+    } else {
+        printf("Rank %i - MPI_Get as expected\n", rank);
     }
 
-    // Start epoch
+    // Put values to another rank
+    MPI_Win_fence(0, window);
+    if(rank < 3) {
+        MPI_Put(putData, NUM_ELEMENT, MPI_INT, putRank, 0, NUM_ELEMENT, MPI_INT, window);
+    }
     MPI_Win_fence(0, window);
 
-    // Put values to neighbour
-    MPI_Put(putData, NUM_ELEMENT, MPI_INT, putRank, 0, NUM_ELEMENT, MPI_INT, window);
-
-    // End epoch
-    MPI_Win_fence(0, window);
-
-    // Check we've had the expected data put in our window
-    if (!faasm::compareIntArrays(rankData, expectedPutData, NUM_ELEMENT)) {
+    // Check we've had the expected data put in our memory
+    if (rank < 3 && !faasm::compareIntArrays(sharedData, expectedPutData, NUM_ELEMENT)) {
         return 1;
+    } else {
+        printf("Rank %i - MPI_Put as expected\n", rank);
     }
 
     MPI_Win_free(&window);
