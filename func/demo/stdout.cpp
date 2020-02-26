252c9122ae3d9a6f24c6fc97da4c933fa53f129f
Simon Shillaker <mail@simonshillaker.com>
2020-02-26 10:08:19 +0000

    Tidy up

--- func/demo/stdout.cpp
+++ func/demo/stdout.cpp
@@ -11,8 +11,10 @@ FAASM_MAIN_FUNC() {
 
     const char* strOut = "I am output";
     printf("Out: %s\n", strOut);
+    fflush(stdout);
 
     printf("Unformatted output\n");
+    fflush(stdout);
 
     faasm::setStringOutput("Normal Faasm output");
 
