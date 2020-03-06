6e14e3580d79087f261e35ca1cf013eb93a200f6
Simon Shillaker <mail@simonshillaker.com>
2020-03-06 14:38:22 +0000

    Tab completion

--- workon.sh
+++ workon.sh
@@ -3,6 +3,10 @@
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd ${THIS_DIR} >> /dev/null
 
+# ----------------------------
+# Virtualenv
+# ----------------------------
+
 if [ ! -d "venv" ]; then
     python3 -m venv venv
 fi
@@ -10,15 +14,32 @@ fi
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 source venv/bin/activate
 
+# ----------------------------
+# Invoke tab-completion (http://docs.pyinvoke.org/en/stable/invoke.html#shell-tab-completion)
+# ----------------------------
+
+_complete_invoke() {
+    local candidates
+    candidates=`invoke --complete -- ${COMP_WORDS[*]}`
+    COMPREPLY=( $(compgen -W "${candidates}" -- $2) )
+}
+
+complete -F _complete_invoke -o default invoke inv
+
+# ----------------------------
+# Environment vars
+# ----------------------------
+
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+
+
 export LOG_LEVEL=debug
-
-export PS1="(faasm) $PS1"
-
 export FAASM_ROOT=$(pwd)
 export FAASM_VERSION=$(cat VERSION)
 export FAASM_CMAKE_TOOLCHAIN=$FAASM_ROOT/toolchain/FaasmToolchain.cmake
 export FAASM_BASH_TOOLCHAIN=$FAASM_ROOT/toolchain/env.sh
+
+export PS1="(faasm) $PS1"
 
 alias fm="inv"
 
