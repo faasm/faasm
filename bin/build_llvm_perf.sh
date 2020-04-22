1ecab19c1cb625b40d19294a20079d906d78fe75
Mayeul Fournial <mfournial@outlook.com>
2020-04-22 14:15:05 +0100

    Update the custom LLVM build

--- bin/build_llvm_perf.sh
+++ bin/build_llvm_perf.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -e
+set -x
 
-FAASM_DIR=${HOME}/faasm
-LLVM_DIR=${FAASM_DIR}/llvm
+THIRD_PARTY_DIR=/usr/local/code/faasm/third-party
+if [[ ! -d ${THIRD_PARTY_DIR} ]]; then
+  echo "THIRD_PARTY_DIR (${THIRD_PARTY_DIR}) does not exists"
+  exit 1
+fi
+
+LLVM_DIR=${THIRD_PARTY_DIR}/llvm-perf
 BUILD_DIR=${LLVM_DIR}/build
 
-# Clone LLVM repo if not done already
-mkdir -p ${FAASM_DIR}
+# Clone LLVM-9 repo if not done already. Version must be compatible with WAVM
 if [[ ! -d "$LLVM_DIR" ]]; then
     git clone https://github.com/llvm/llvm-project ${LLVM_DIR}
     pushd ${LLVM_DIR}
@@ -14,17 +19,20 @@ if [[ ! -d "$LLVM_DIR" ]]; then
     popd
 fi
 
-# Clear build dir
-# rm -rf ${BUILD_DIR}
-
-# Make the build dir if it exists
+# Make the build dir
 mkdir -p ${BUILD_DIR}
-
 pushd ${BUILD_DIR}
+
+# Clear build dir
+if [[ -f "${BUILD_DIR}/build.ninja" ]]; then
+  ninja clean
+fi
 
 # Enable JIT profiling features of LLVM
 cmake \
 	-G Ninja \
+	-DCMAKE_C_COMPILER=/usr/bin/clang \
+	-DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
 	-DCMAKE_BUILD_TYPE=RelWithDebInfo \
 	-DLLVM_USE_PERF=1 \
 	-DLLVM_USE_INTEL_JITEVENTS=1 \
