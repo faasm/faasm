21d2044b87db2d82a01a0e1d4924e4a78a321905
Mayeul Fournial <mfournial@outlook.com>
2020-04-23 18:20:13 +0100

    Clean up mt_pi function

--- func/omp/mt_pi.cpp
+++ func/omp/mt_pi.cpp
@@ -2,8 +2,6 @@
 #include <cstdio>
 #include <random>
 
-//#define num_devices num_threads
-
 unsigned long thread_seed() {
     int threadNum = omp_get_thread_num();
     return threadNum * threadNum * 77 - 22 * threadNum + 1927;
@@ -14,12 +12,10 @@ int main(int argc, char **argv) {
     int num_threads = 1;
     if (argc == 3) {
         num_threads = std::stoi(argv[1]);
-        iterations = std::stol(argv[2]);
+        iterations = std::stoll(argv[2]);
     } else if (argc != 1) {
         printf("Usage: mt_pi [num_threads num_iterations]");
     }
-
-//    omp_set_default_device(-2);
 
     long long result = 0;
     #pragma omp parallel num_threads(num_threads) default(none) firstprivate(iterations) reduction(+:result)
@@ -42,5 +38,4 @@ int main(int argc, char **argv) {
     if (pi - 3.14 > 0.01) {
         printf("Low accuracy. Expected pi got %f\n", pi);
     }
-    return EXIT_SUCCESS;
 }
