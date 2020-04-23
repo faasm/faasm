e5f3d03d97038c50f3371b6723edeef8564b2102
Mayeul Fournial <mfournial@outlook.com>
2020-04-23 18:20:30 +0100

    Change OMP benchmarker to use docker version

--- src/runner/bench_omp.cpp
+++ src/runner/bench_omp.cpp
@@ -6,107 +6,106 @@
 #include <util/logging.h>
 #include <util/timing.h>
 
-constexpr auto BINARY_DIR = "/usr/local/code/faasm/ninja-build/bin/";
+constexpr auto DOCKER_CMD = "/usr/bin/docker run --cpus=20 -v /usr/local/code/faasm/wasm:/wasm --rm faasm/omp-musl:0.1.3 docker_benchmarker ";
 
-void
-nativeRun(std::ofstream &profOut, const std::string &execPath, long num_iterations, const std::string &iteration_name,
-          int num_threads);
+void nativeRun(const std::string &funcName, int numRuns);
 
-void wasmRun(std::ofstream &profOut, message::Message &call, long num_iterations, const std::string &iteration_name,
-             int num_threads, wasm::WAVMWasmModule module);
+void wasmRun(const std::string &user, const std::string &funcName, int wasmIterations, const std::string &outfile);
+void wasmRunIteration(std::ofstream &profOut, message::Message &call, long num_iterations, const std::string &iteration_name,
+                      int num_threads, wasm::WAVMWasmModule module);
 
 int main(int argc, char *argv[]) {
     util::initLogging();
     const std::shared_ptr<spdlog::logger> &logger = util::getLogger();
 
-    if (argc < 4) {
-        logger->error("Usage:\nbench_func <user> <function> <nNative> <nWasm>");
+    if (argc != 4) {
+        logger->error("Usage:\nbench_omp <function> <nNative> <nWasm>");
         return 1;
     }
 
-    std::string user = argv[1];
-    std::string funcName = argv[2];
-    int nativeIterations = std::stoi(argv[3]);
-    int wasmIterations = std::stoi(argv[4]);
+    std::string user = "omp";
+    std::string funcName = argv[1];
+    int nativeIterations = std::stoi(argv[2]);
+    int wasmIterations = std::stoi(argv[3]);
+
+    std::string outfile = std::string("/usr/local/code/faasm/wasm/omp/")  + funcName + "/bench.csv";
+    // Clean up for container (and ensuring we will be writing to the same file
+    boost::filesystem::remove(outfile);
 
     logger->info("Benchmarking {} ({}x native and {}x wasm)", funcName, nativeIterations, wasmIterations);
 
-    std::string outfile = std::string("/usr/local/code/faasm/wasm/") + user + "/" + funcName + "/bench.csv";
+    nativeRun(funcName, nativeIterations);
+    wasmRun(user, funcName, wasmIterations, outfile);
+
+    logger->info("Finished benchmark - {}", funcName);
+    return 0;
+}
+
+void nativeRun(const std::string &funcName, int numRuns) {
+    auto logger = util::getLogger();
+    const std::string execCmd = fmt::format("{} {} {}", DOCKER_CMD, funcName, numRuns);
+
+    logger->info("Exectuting native");
+
+    const util::TimePoint nativeTp = util::startTimer();
+    const int error = system(execCmd.c_str());
+    const long nativeTime = util::getTimeDiffMillis(nativeTp);
+
+    logger->info("Done executing native in {} ms", nativeTime);
+
+    if (error != 0) {
+        logger->error("Failed to execute %s", execCmd.c_str());
+    }
+}
+
+void wasmRun(const std::string &user, const std::string &funcName, int wasmIterations, const std::string &outfile) {
+
+    const std::shared_ptr<spdlog::logger> &logger = util::getLogger();
 
     std::ofstream profOut;
-    profOut.open(outfile);
-    profOut << "iterations,numThreads,type,microseconds" << std::endl;
+    profOut.open(outfile, std::fstream::app);
 
-    std::string execPath = std::string(BINARY_DIR) + funcName;
-
-    logger->info("Running benchmark natively");
-    if (!boost::filesystem::exists(execPath)) {
-        throw std::runtime_error("Could not find binary at " + execPath);
+    logger->info("Running Wasm benchmark");
+    if (!boost::filesystem::exists(outfile)) {
+        throw std::runtime_error("Could not find native benchmark output at " + outfile);
     }
 
     std::vector<long> iterations = {200000l, 20000000l, 200000000l};
     std::vector<std::string> iter_names = {"Tiny,", "Small,", "Big,"};
 
-    for (int run = 1; run <= nativeIterations; run++) {
-        logger->info("NATIVE - {} ({}/{})", funcName, run, nativeIterations);
-        for (size_t i = 0; i < iterations.size(); i++) {
-            nativeRun(profOut, execPath, iterations[i], iter_names[i], 1);
-            for (int num_threads = 2; num_threads < 25; num_threads += 2) {
-                nativeRun(profOut, execPath, iterations[i], iter_names[i], num_threads);
-            }
-        }
-    }
-
     message::Message call = util::messageFactory(user, funcName);
     module_cache::WasmModuleCache &moduleCache = module_cache::getWasmModuleCache();
     wasm::WAVMWasmModule &cachedModule = moduleCache.getCachedModule(call);
 
+    const util::TimePoint wasmTp = util::startTimer();
+
     for (int run = 1; run <= wasmIterations; run++) {
-        logger->info("WASM - {} ({}/{})", funcName, run, nativeIterations);
+        logger->info("WASM - {} ({}/{})", funcName, run, wasmIterations);
         for (size_t i = 0; i < iterations.size(); i++) {
-            wasmRun(profOut, call, iterations[i], iter_names[i], 1, cachedModule);
+            wasmRunIteration(profOut, call, iterations[i], iter_names[i], 1, cachedModule);
             for (int num_threads = 2; num_threads < 25; num_threads += 2) {
-                wasmRun(profOut, call, iterations[i], iter_names[i], num_threads, cachedModule);
+                wasmRunIteration(profOut, call, iterations[i], iter_names[i], num_threads, cachedModule);
             }
         }
     }
 
+    const long wasmTime = util::getTimeDiffMillis(wasmTp);
+    logger->info("Done executing native in {} ms", wasmTime);
 
-    logger->info("Finished benchmark - {}", funcName);
 
     profOut.flush();
     profOut.close();
-
-    return 0;
 }
 
-void wasmRun(std::ofstream &profOut, message::Message &call, long num_iterations, const std::string &iteration_name,
+void wasmRunIteration(std::ofstream &profOut, message::Message &call, long num_iterations, const std::string &iteration_name,
              int num_threads, wasm::WAVMWasmModule cachedModule) {
     auto args = fmt::format("{} {}", num_threads, num_iterations);
     call.set_cmdline(args.c_str());
-    const util::TimePoint wasmTp = util::startTimer();
 
+    const util::TimePoint iterationTp = util::startTimer();
     wasm::WAVMWasmModule module{cachedModule};
-
     module.execute(call);
+    const long wasmIterationTime = util::getTimeDiffMicros(iterationTp);
 
-    const long nativeTime = util::getTimeDiffMicros(wasmTp);
-
-    profOut << iteration_name << num_threads << ",wasm," << nativeTime << std::endl;
-}
-
-void
-nativeRun(std::ofstream &profOut, const std::string &execPath, long num_iterations, const std::string &iteration_name,
-          int num_threads) {
-    const std::string execCmd = fmt::format("{} {} {}", execPath, num_threads, num_iterations);
-
-    const util::TimePoint wasmTp = util::startTimer();
-    const int error = system(execCmd.c_str());
-    const long nativeTime = util::getTimeDiffMicros(wasmTp);
-
-    if (error != 0) {
-        printf("Failed to execute %s", execCmd.c_str());
-    }
-
-    profOut << iteration_name << num_threads << ",native," << nativeTime << std::endl;
+    profOut << iteration_name << num_threads << ",wasm," << wasmIterationTime << std::endl;
 }
