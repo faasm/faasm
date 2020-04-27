35b32b2e50117654fa826a0dca9563e5fd9d9dc4
Mike Fournial <mfournial@outlook.com>
2020-04-27 10:42:19 +0100

    Fixes toolchain for the release (#233)
    * Fix Toolchain compilation
    
    * Fix lib-cpp macro

--- libs/cpp/faasm/faasm.h
+++ libs/cpp/faasm/faasm.h
@@ -6,7 +6,7 @@
 // For different integrations we need to provide different entry
 // points. These must define their own main() method which ensures
 // exec() eventually gets called
-if KNATIVE_NATIVE == 1
+#if KNATIVE_NATIVE == 1
 #include <knative_native/interface.h>
 #else
 
