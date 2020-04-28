5e547046964ed392aebf2e82dd9d9ede2fd0f753
Mayeul Fournial <mfournial@outlook.com>
2020-04-28 16:17:04 +0100

    Start development branch

--- src/wavm/openmp.cpp
+++ src/wavm/openmp.cpp
@@ -279,7 +279,6 @@ namespace wasm {
                     }
                 }
 
-
                 // Snapshot details
                 call.set_snapshotkey(activeSnapshotKey);
                 call.set_snapshotsize(threadSnapshotSize);
