8da7dbdf90e36dfd3ce1a0aa4114f01e5c0efc72
Simon Shillaker <mail@simonshillaker.com>
2020-06-01 16:42:00 +0100

    Fix native function container gners

--- src/knative_native/knative_native_runner.cpp
+++ src/knative_native/knative_native_runner.cpp
@@ -3,6 +3,6 @@
 // This is a dummy placeholder that should only get executed
 // for testing purposes
 FAASM_MAIN_FUNC() {
-    printf("I should not be getting executed\n");
+    printf("I am a testing function\n");
     return 0;
 }
