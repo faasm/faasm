0e95f23bcb8f8137caaf165749be5872198bdebb
Joshua Heinemann <heineman@ibr.cs.tu-bs.de>
2020-06-05 16:34:44 +0200

    WAMR preparation

--- src/sgx/sgx_runner_enclave.cpp
+++ src/sgx/sgx_runner_enclave.cpp
@@ -2,9 +2,11 @@
 // Created by Joshua Heinemann on 04.06.20.
 // TU-Braunschweig (heineman@ibr.cs.tu-bs.de)
 //
+#include <wasm_export.h>
 extern "C"{
     extern void ocall_printf(char* msg);
 void enclave_main(void){
     ocall_printf("Hello from enclave side.");
+
 }
 };
