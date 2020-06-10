26493639efa9cc979409f167c5ffdfe766bf2aa3
Joshua Heinemann <heineman@ibr.cs.tu-bs.de>
2020-06-10 23:44:51 +0200

    SGX: WAMR integration

--- src/sgx/sgx_runner.cpp
+++ src/sgx/sgx_runner.cpp
@@ -10,26 +10,71 @@
 #include <stdio.h>
 #include <sgx.h>
 #include <sgx_urts.h>
+#include <sgx/faasm_sgx_error.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 
 extern "C"{
-void ocall_printf(char* message){
-    printf("[Enclave] %s\n",message);
+void ocall_printf(const char* message){
+    printf("[Enclave] %s",message);
 }
-extern sgx_status_t enclave_main(sgx_enclave_id_t);
+extern sgx_status_t enclave_call_function(sgx_enclave_id_t enclave_id, faasm_sgx_status_t* ret_val, const char* wasm_function_name, uint32_t wasm_function_argc, uint32_t* wasm_function_argv);
+extern sgx_status_t enclave_load_module(sgx_enclave_id_t enclave_id, faasm_sgx_status_t* ret_val, void* wasm_opcode_ptr, uint32_t wasm_opcode_size);
+extern sgx_status_t enclave_init_wamr(sgx_enclave_id_t enclave_id, faasm_sgx_status_t* ret_val);
 };
 int main(int argc, char** argv){
     sgx_enclave_id_t enclave_id;
     sgx_launch_token_t enclave_token = {0};
     int sgx_launch_token_updated = 0;
     sgx_status_t enclave_ret_val;
+    //!!! TEST CODE !!!/////!!! TEST CODE !!!/////!!! TEST CODE !!!/////!!! TEST CODE !!!/////!!! TEST CODE !!!/////!!! TEST CODE !!!///
+    int fd;
+    struct stat hello_world_fstat;
+    faasm_sgx_status_t ret_val;
+    memset(&hello_world_fstat,0x0,sizeof(struct stat));
+    if((fd = open("bin/hello_world.wasm",O_RDONLY)) == -1){
+        perror("open");
+        return -1;
+    }
+    fstat(fd,&hello_world_fstat);
+    void* hello_world_ptr = NULL;
+    if((hello_world_ptr = mmap(NULL,hello_world_fstat.st_size,PROT_READ|PROT_WRITE|PROT_EXEC,MAP_PRIVATE,fd,0x0)) == (void*)-1){
+        perror("mmap");
+        return -1;
+    }
     printf("[Info] Hello World from sgx_runner\n");
-
     if((enclave_ret_val = sgx_create_enclave(SGX_RUNNER_ENCLAVE_PATH,SGX_DEBUG_FLAG,&enclave_token,&sgx_launch_token_updated,&enclave_id,NULL)) != SGX_SUCCESS){
         printf("[Error] Unable to create enclave (%d).\n",enclave_ret_val);
         return 0;
     }
-    if((enclave_ret_val = enclave_main(enclave_id)) != SGX_SUCCESS){
+    if((enclave_ret_val = enclave_init_wamr(enclave_id,&ret_val)) != SGX_SUCCESS){
+        printf("[Error] enclave_init_wamr (%d)\n",enclave_ret_val);
+        return -1;
+    }
+    if(ret_val != FAASM_SGX_SUCCESS){
+        printf("GRande error\n");
+    }
+    if((enclave_ret_val = enclave_load_module(enclave_id,&ret_val,hello_world_ptr,hello_world_fstat.st_size)) != SGX_SUCCESS){
+        printf("[Error] Unable to enter enclave (%d).\n",enclave_ret_val);
+        return -1;
+    }
+    if(ret_val != FAASM_SGX_SUCCESS){
+        printf("Error %d\n",ret_val);
+        return -1;
+    }
+    uint32_t func_parameter[] ={
+            {NULL},
+            {NULL}
+    };
+    if((enclave_ret_val = enclave_call_function(enclave_id,&ret_val,"main",2,func_parameter)) != SGX_SUCCESS){
         printf("[Error] Unable to enter enclave (%d).\n",enclave_ret_val);
     }
+    if(ret_val != FAASM_SGX_SUCCESS){
+        printf("Error %d\n",ret_val);
+        return -1;
+    }
     return 0;
+    //!!! TEST CODE !!!/////!!! TEST CODE !!!/////!!! TEST CODE !!!/////!!! TEST CODE !!!/////!!! TEST CODE !!!/////!!! TEST CODE !!!///
 }
--- src/sgx/sgx_runner_enclave.cpp
+++ src/sgx/sgx_runner_enclave.cpp
@@ -2,11 +2,83 @@
 // Created by Joshua Heinemann on 04.06.20.
 // TU-Braunschweig (heineman@ibr.cs.tu-bs.de)
 //
-#include <wasm_export.h>
-extern "C"{
-    extern void ocall_printf(char* msg);
-void enclave_main(void){
-    ocall_printf("Hello from enclave side.");
 
-}
+#include <sgx.h>
+#include <sgx_defs.h>
+#include <sgx/faasm_sgx_error.h>
+#include <string.h>
+#include <wasm_export.h>
+
+#ifndef FAASM_SGX_WAMR_HEAP_SIZE
+#define FAASM_SGX_WAMR_HEAP_SIZE 256 //Default: 256KB
+#endif
+
+#ifndef FAASM_SGX_WAMR_MODULE_ERROR_BUFFER_SIZE
+#define FAASM_SGX_WAMR_MODULE_ERROR_BUFFER_SIZE 128
+#endif
+
+#ifndef FAASM_SGX_WAMR_INSTANCE_DEFAULT_HEAP_SIZE
+#define FAASM_SGX_WAMR_INSTANCE_DEFAULT_HEAP_SIZE 8196
+#endif
+
+#ifndef FAASM_SGX_WAMR_INSTANCE_DEFAULT_STACK_SIZE
+#define FAASM_SGX_WAMR_INSTANCE_DEFAULT_STACK_SIZE 8196
+#endif
+
+extern "C"{
+    extern sgx_status_t SGX_CDECL ocall_printf(const char* message);
+    static char wamr_global_heap_buffer[FAASM_SGX_WAMR_HEAP_SIZE * 1024];
+    static wasm_exec_env_t wamr_global_exec_env;
+    static wasm_module_inst_t wamr_global_module_instance;
+    void ocall_printf_wrapper(wasm_exec_env_t exec_env, const char* msg){
+        ocall_printf(msg); //TODO: IMPROVE PRINTF
+    }
+    faasm_sgx_status_t enclave_call_function(const char* wasm_function_name, uint32_t wasm_function_argc, uint32_t* wasm_function_argv){
+        wasm_function_inst_t wasm_function;
+        if(!wasm_function_name)
+            return FAASM_SGX_INVALID_PTR;
+        if(!(wasm_function = wasm_runtime_lookup_function(wamr_global_module_instance,wasm_function_name,NULL)))
+            return FAASM_SGX_WAMR_FUNCTION_NOT_FOUND;
+        if(!(wasm_runtime_call_wasm(wamr_global_exec_env,wasm_function,wasm_function_argc,wasm_function_argv))){
+            const char* error = wasm_runtime_get_exception(wamr_global_module_instance); //TODO: MAKE THIS MORE PRETTY
+            ocall_printf(error);
+            return FAASM_SGX_WAMR_FUNCTION_UNABLE_TO_CALL;
+        }
+        return FAASM_SGX_SUCCESS;
+    }
+    faasm_sgx_status_t enclave_load_module(void* wasm_opcode_ptr, uint32_t wasm_opcode_size){
+        wasm_module_t wasm_module;
+        char module_error_buffer[FAASM_SGX_WAMR_MODULE_ERROR_BUFFER_SIZE];
+        memset(module_error_buffer, 0x0, sizeof(module_error_buffer));
+        if(!wasm_opcode_ptr)
+            return FAASM_SGX_INVALID_PTR;
+        if(!(wasm_module = wasm_runtime_load((uint8_t*)wasm_opcode_ptr,wasm_opcode_size,module_error_buffer,sizeof(module_error_buffer)))){
+            ocall_printf(module_error_buffer);
+            return FAASM_SGX_WAMR_MODULE_LOAD_FAILED;
+        }
+        if(!(wamr_global_module_instance = wasm_runtime_instantiate(wasm_module,(uint32_t)FAASM_SGX_WAMR_INSTANCE_DEFAULT_STACK_SIZE,(uint32_t)FAASM_SGX_WAMR_INSTANCE_DEFAULT_HEAP_SIZE,module_error_buffer,sizeof(module_error_buffer)))){
+            ocall_printf(module_error_buffer);
+            return FAASM_SGX_WAMR_MODULE_INSTANTIATION_FAILED;
+        }
+        if(!(wamr_global_exec_env = wasm_runtime_create_exec_env(wamr_global_module_instance,FAASM_SGX_WAMR_INSTANCE_DEFAULT_STACK_SIZE))){
+            return FAASM_SGX_WAMR_EXECUTION_ENVIRONMENT_GENERATION_FAILED;
+        }
+        return FAASM_SGX_SUCCESS;
+    }
+    faasm_sgx_status_t enclave_init_wamr(void) {
+        static NativeSymbol wamr_runtime_symbols[] = {
+                {"printf",(void*)ocall_printf_wrapper,"($)"}
+        };
+        RuntimeInitArgs wamr_runtime_init_args;
+        memset(&wamr_runtime_init_args, 0x0, sizeof(wamr_runtime_init_args));
+        wamr_runtime_init_args.mem_alloc_type = Alloc_With_Pool;
+        wamr_runtime_init_args.mem_alloc_option.pool.heap_buf = wamr_global_heap_buffer;
+        wamr_runtime_init_args.mem_alloc_option.pool.heap_size = sizeof(wamr_global_heap_buffer);
+        wamr_runtime_init_args.native_module_name = "env";
+        wamr_runtime_init_args.native_symbols = wamr_runtime_symbols;
+        wamr_runtime_init_args.n_native_symbols = sizeof(wamr_runtime_symbols) / sizeof(NativeSymbol);
+        if(!wasm_runtime_full_init(&wamr_runtime_init_args))
+            return FAASM_SGX_WAMR_RTE_INIT_FAILED;
+        return FAASM_SGX_SUCCESS;
+    }
 };
