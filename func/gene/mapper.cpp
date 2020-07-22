cbdf735bf1bde1de24eaef305a3c4aa853a59261
Simon Shillaker <s.shillaker17@imperial.ac.uk>
2020-07-22 14:52:53 +0000

    Add in all genome index chunks

--- func/gene/mapper.cpp
+++ func/gene/mapper.cpp
@@ -5,8 +5,12 @@
 #include <string>
 
 // Index chunks
-#define N_CHUNKS 1
-static int indexChunks[N_CHUNKS] = {24};
+#define N_CHUNKS 24
+static int indexChunks[N_CHUNKS] = {
+	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+	11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+	21, 22, 23, 24
+};
 
 /*
  * This function fans out the mapping for a given chunk of reads and awaits the results.
