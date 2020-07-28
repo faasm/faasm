6c3146843ef2cd4f63cb9540b1cd58a7f6575c36
Simon Shillaker <mail@simonshillaker.com>
2020-07-28 17:57:21 +0200

    Version bump

--- faasmcli/faasmcli/tasks/ibm.py
+++ faasmcli/faasmcli/tasks/ibm.py
@@ -78,7 +78,7 @@ def ibm_deploy_worker(ctx, update=False):
         "--concurrency 20",
         "faasm-worker",
         "--docker",
-        "faasm/ibm-worker:0.2.4"
+        "faasm/ibm-worker:0.2.5"
     ]
 
     cmd_string = " ".join(cmd)
