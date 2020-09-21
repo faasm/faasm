4df66f4bac3dfdb3ffbfa687151131c88ff26722
Simon Shillaker <scs17@ic.ac.uk>
2020-09-21 10:52:09 +0000

    Moving to new CPython build

--- faasmcli/faasmcli/tasks/python.py
+++ faasmcli/faasmcli/tasks/python.py
@@ -1,41 +1,23 @@
 from os.path import join, exists
-from shutil import rmtree
-from subprocess import check_output
+from shutil import rmtree, copytree
+from subprocess import run
+from os import makedirs
 
 from invoke import task
 
 from faasmcli.util import codegen as cg
 from faasmcli.util.env import (
-    PYODIDE_INSTALL_DIR,
     FAASM_RUNTIME_ROOT,
+    THIRD_PARTY_DIR,
     PY_RUNTIME_ROOT,
-    PYODIDE_PACKAGES,
-    FAASM_SHARED_ROOT,
 )
 from faasmcli.util.files import glob_remove
 
-# TODO - avoid having to hard-code this
-_PACKAGES_INCLUDED = {
-    "dulwich": {
-        "path": "dulwich/build/dulwich-0.19.11/install/lib/python3.7/site-packages/dulwich",
-    },
-    "genshi": {
-        "path": "Genshi/build/Genshi-0.7.2/install/lib/python3.7/site-packages/genshi",
-    },
-    "numpy": {
-        "path": "numpy/build/numpy-1.15.1/install/lib/python3.7/site-packages/numpy",
-    },
-    "pyperformance": {
-        "path": "pyperformance/build/pyperformance-1.0.0/install/lib/python3.7/site-packages/pyperformance",
-    },
-    "pyaes": {
-        "path": "pyaes/build/pyaes-1.6.1/install/lib/python3.7/site-packages/pyaes",
-    },
-    "pyfaasm": {
-        "path": "pyfaasm/build/pyfaasm-*/install/lib/python3.7/site-packages/pyfaasm",
-    },
-    "six": {"path": "six/build/six-1.12.0/install/lib/python3.7/site-packages/six.py"},
-}
+
+FAASM_CPYTHON_DIR = join(THIRD_PARTY_DIR, "faasm-cpython")
+CPYTHON_SRC = join(FAASM_CPYTHON_DIR, "third-party", "cpython")
+CPYTHON_INSTALL_DIR = join(CPYTHON_SRC, "install", "wasm")
+CROSSENV_WASM_DIR = join(FAASM_CPYTHON_DIR, "cross_venv", "cross")
 
 
 def _clear_pyc_files(dir_path):
@@ -53,6 +35,72 @@ def _remove_runtime_dir(dir_name):
 
 
 @task
+def copy_cpython(ctx):
+    """
+    Copies the CPython archive and all installed modules from the Faasm cpython
+    build into the Faasm runtime root
+    """
+
+    include_root = join(FAASM_RUNTIME_ROOT, "include")
+    lib_root = join(FAASM_RUNTIME_ROOT, "lib")
+
+    if not exists(lib_root):
+        print("Creating {}".format(lib_root))
+        makedirs(lib_root)
+
+    if not exists(include_root):
+        print("Creating {}".format(include_root))
+        makedirs(include_root)
+
+    include_src_dir = join(CPYTHON_INSTALL_DIR, "include", "python3.8")
+    lib_src_dir = join(CPYTHON_INSTALL_DIR, "lib", "python3.8")
+    site_packages_src_dir = join(
+        CROSSENV_WASM_DIR, "lib", "python3.8", "site-packages"
+    )
+
+    include_dest_dir = join(include_root, "python3.8")
+    lib_dest_dir = join(lib_root, "python3.8")
+    site_packages_dest_dir = join(lib_dest_dir, "site-packages")
+
+    # Clear out pyc files
+    print("Clearing out pyc files")
+    _clear_pyc_files(lib_src_dir)
+    _clear_pyc_files(site_packages_src_dir)
+
+    # Remove dirs to be replaced by those we copy in
+    if exists(include_dest_dir):
+        print("Removing {}".format(include_dest_dir))
+        rmtree(include_dest_dir)
+
+    if exists(lib_dest_dir):
+        print("Removing {}".format(lib_dest_dir))
+        rmtree(lib_dest_dir)
+
+    # Copy CPython includes
+    print("Copying {} to {}".format(include_src_dir, include_dest_dir))
+    copytree(include_src_dir, include_dest_dir)
+
+    # Copy CPython libs
+    print("Copying {} to {}".format(lib_src_dir, lib_dest_dir))
+    copytree(lib_src_dir, lib_dest_dir)
+
+    # Copy cross-compiled modules
+    if not exists(site_packages_dest_dir):
+        makedirs(site_packages_dest_dir)
+
+    print(
+        "Copying {} to {}".format(
+            site_packages_src_dir, site_packages_dest_dir
+        )
+    )
+    run(
+        "cp -r {}/* {}/".format(site_packages_src_dir, site_packages_dest_dir),
+        shell=True,
+        check=True,
+    )
+
+
+@task
 def clear_runtime_pyc(ctx):
     """
     Clear out runtime .pyc files
@@ -62,68 +110,24 @@ def clear_runtime_pyc(ctx):
 
 
 @task
-def set_up_package(ctx, pkg_name):
-    """
-    Set up a given Python package
-    """
-    _do_set_up_python_packages([pkg_name])
-
-    # Run codegen for just this package
-    binary = cg.find_codegen_shared_lib()
-    pkg_install_dir = join(PY_RUNTIME_ROOT, "site-packages", pkg_name)
-
-    if not exists(pkg_install_dir):
-        print("Expected to find package installed at {}".format(pkg_install_dir))
-        return 1
-
-    check_output("{} {}".format(binary, pkg_install_dir), shell=True)
-
-
-def _do_set_up_python_packages(package_names):
-    for pkg_name in package_names:
-        print("\n --------- {} ---------".format(pkg_name))
-
-        pkg_detail = _PACKAGES_INCLUDED[pkg_name]
-        pkg_dir = join(PYODIDE_PACKAGES, pkg_detail["path"])
-
-        # Put files in place
-        print("Copying {} into place".format(pkg_name))
-        runtime_site_packages = join(PY_RUNTIME_ROOT, "site-packages")
-        check_output("cp -r {} {}".format(pkg_dir, runtime_site_packages), shell=True)
-
-
-@task
-def set_up_runtime(ctx):
+def runtime(ctx):
     """
     Set up the Python runtime
     """
-    print("Clearing out pyc files")
-    _clear_pyc_files(PYODIDE_INSTALL_DIR)
-    _clear_pyc_files(PYODIDE_PACKAGES)
-
-    print("\nRemoving any existing runtime files")
-    _remove_runtime_dir("include")
-    _remove_runtime_dir("lib")
+    print("Copying the CPython runtime files into place")
+    copy_cpython(ctx)
 
     print("\nRemoving any existing python functions")
     funcs_dir = join(FAASM_RUNTIME_ROOT, "pyfuncs")
     if exists(funcs_dir):
         rmtree(funcs_dir)
 
-    print("\nPutting CPython libraries in place")
-    check_output(
-        "cp -r {}/* {}".format(PYODIDE_INSTALL_DIR, FAASM_RUNTIME_ROOT), shell=True
+    # Create a tmp directory
+    run(
+        "mkdir -p {}".format(join(PY_RUNTIME_ROOT, "tmp")),
+        shell=True,
+        check=True,
     )
-
-    # Set up files in runtime root
-    runtime_site_packages = join(PY_RUNTIME_ROOT, "site-packages")
-    check_output("mkdir -p {}".format(runtime_site_packages), shell=True)
-
-    # Create a tmp directory for work
-    check_output("mkdir -p {}".format(join(PY_RUNTIME_ROOT, "tmp")), shell=True)
-
-    print("\nSetting up packages")
-    _do_set_up_python_packages(_PACKAGES_INCLUDED.keys())
 
     # Run codegen
     codegen(ctx)
@@ -135,4 +139,5 @@ def codegen(ctx):
     Run Python codegen
     """
     binary = cg.find_codegen_shared_lib()
-    check_output("{} {}".format(binary, PY_RUNTIME_ROOT), shell=True)
+    run("{} {}".format(binary, PY_RUNTIME_ROOT), shell=True, check=True)
+
