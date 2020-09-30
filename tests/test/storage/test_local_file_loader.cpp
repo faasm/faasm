49895ae9a86859d1e01213618bc4091487ff3362
Simon Shillaker <scs17@ic.ac.uk>
2020-09-30 11:39:30 +0000

    Tests

--- tests/test/storage/test_local_file_loader.cpp
+++ tests/test/storage/test_local_file_loader.cpp
@@ -1,30 +1,32 @@
 #include <catch/catch.hpp>
 
-#include <stdlib.h>
-#include <storage/LocalFileLoader.h>
 #include <boost/filesystem.hpp>
 #include <faabric/util/files.h>
+#include <stdlib.h>
+#include <storage/LocalFileLoader.h>
 
 using namespace storage;
 
 namespace tests {
-    TEST_CASE("Check uploading and loading shared files", "[storage]") {
-        std::string relativePath = "test/local_file_loader.txt";
-        std::vector<uint8_t> expected = {1, 5, 3, 2, 4};
+TEST_CASE("Check uploading and loading shared files", "[storage]")
+{
+    std::string relativePath = "test/local_file_loader.txt";
+    std::vector<uint8_t> expected = { 1, 5, 3, 2, 4 };
 
-        storage::FileLoader &loader = storage::getFileLoader();
-        loader.uploadSharedFile(relativePath, expected);
+    storage::FileLoader& loader = storage::getFileLoader();
+    loader.uploadSharedFile(relativePath, expected);
 
-        const std::vector<uint8_t> actual = loader.loadSharedFile(relativePath);
+    const std::vector<uint8_t> actual = loader.loadSharedFile(relativePath);
 
-        REQUIRE(actual == expected);
-        
-        // Check it's written where we expect it to be too
-        faabric::util::SystemConfig &conf = faabric::util::getSystemConfig();
-        boost::filesystem::path fullPath(conf.sharedFilesStorageDir);
-        fullPath.append(relativePath);
+    REQUIRE(actual == expected);
 
-        const std::vector<uint8_t> actualBytes = faabric::util::readFileToBytes(fullPath.string());
-        REQUIRE(actualBytes == expected);
-    }
+    // Check it's written where we expect it to be too
+    faabric::util::SystemConfig& conf = faabric::util::getSystemConfig();
+    boost::filesystem::path fullPath(conf.sharedFilesStorageDir);
+    fullPath.append(relativePath);
+
+    const std::vector<uint8_t> actualBytes =
+      faabric::util::readFileToBytes(fullPath.string());
+    REQUIRE(actualBytes == expected);
+}
 }
--- tests/test/storage/test_shared_files.cpp
+++ tests/test/storage/test_shared_files.cpp
@@ -4,51 +4,55 @@
 
 #include <boost/filesystem.hpp>
 
-#include <storage/SharedFiles.h>
 #include <faabric/util/files.h>
+#include <storage/SharedFiles.h>
 
 using namespace storage;
 
 namespace tests {
-    TEST_CASE("Check sync shared file", "[storage]") {
-        SharedFiles::clear();
+TEST_CASE("Check sync shared file", "[storage]")
+{
+    SharedFiles::clear();
 
-        faabric::util::SystemConfig &conf = faabric::util::getSystemConfig();
-        std::string relPath = "shared_test_dir/dummy_file.txt";
-        std::string sharedPath = "faasm://" + relPath;
-        std::string realPath = faabric::util::getSharedFileFile(relPath);
-        
-        std::string syncedPath;
-        std::string overridePath;
+    faabric::util::SystemConfig& conf = faabric::util::getSystemConfig();
+    std::string relPath = "shared_test_dir/dummy_file.txt";
+    std::string sharedPath = "faasm://" + relPath;
+    std::string realPath = faabric::util::getSharedFileFile(relPath);
 
-        SECTION("Default location") {
-            syncedPath = conf.sharedFilesDir + "/" + relPath;
-            overridePath = "";
-        }
+    std::string syncedPath;
+    std::string overridePath;
 
-        SECTION("Custom location") {
-            overridePath = "/tmp/shared_file_test.txt";
-            syncedPath = overridePath;
-        }
-
-        std::vector<uint8_t> bytes = {0, 1, 2, 3, 4, 5};
-
-        // Set up file
-        if(boost::filesystem::exists(realPath)) {
-            boost::filesystem::remove(realPath);
-        }
-        faabric::util::writeBytesToFile(realPath, bytes);
-
-        if(boost::filesystem::exists(syncedPath)) {
-            boost::filesystem::remove(syncedPath);
-        }
-        
-        // Do the sync
-        SharedFiles::syncSharedFile(sharedPath, overridePath);
-        
-        // Check the file is now present and contents match
-        REQUIRE(boost::filesystem::exists(syncedPath));
-        std::vector<uint8_t> actualBytes = faabric::util::readFileToBytes(syncedPath);
-        REQUIRE(actualBytes == bytes);
+    SECTION("Default location")
+    {
+        syncedPath = conf.sharedFilesDir + "/" + relPath;
+        overridePath = "";
     }
+
+    SECTION("Custom location")
+    {
+        overridePath = "/tmp/shared_file_test.txt";
+        syncedPath = overridePath;
+    }
+
+    std::vector<uint8_t> bytes = { 0, 1, 2, 3, 4, 5 };
+
+    // Set up file
+    if (boost::filesystem::exists(realPath)) {
+        boost::filesystem::remove(realPath);
+    }
+    faabric::util::writeBytesToFile(realPath, bytes);
+
+    if (boost::filesystem::exists(syncedPath)) {
+        boost::filesystem::remove(syncedPath);
+    }
+
+    // Do the sync
+    SharedFiles::syncSharedFile(sharedPath, overridePath);
+
+    // Check the file is now present and contents match
+    REQUIRE(boost::filesystem::exists(syncedPath));
+    std::vector<uint8_t> actualBytes =
+      faabric::util::readFileToBytes(syncedPath);
+    REQUIRE(actualBytes == bytes);
+}
 }
