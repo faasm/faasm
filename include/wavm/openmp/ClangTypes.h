b4dd1a36adab5c8bec217b00ce90be19289b2f46
Simon Shillaker <scs17@ic.ac.uk>
2020-09-30 11:38:15 +0000

    include/wavm

--- include/wavm/openmp/ClangTypes.h
+++ include/wavm/openmp/ClangTypes.h
@@ -2,11 +2,12 @@
 
 // Types in accordance with Clang's OpenMP implementation
 namespace wasm {
-    namespace kmp {
-        enum sched_type : int {
-            sch_lower = 32, /**< lower bound for unordered values */
-            sch_static_chunked = 33,
-            sch_static = 34, /**< static unspecialized */
-        };
-    }
+namespace kmp {
+enum sched_type : int
+{
+    sch_lower = 32, /**< lower bound for unordered values */
+    sch_static_chunked = 33,
+    sch_static = 34, /**< static unspecialized */
+};
+}
 }
