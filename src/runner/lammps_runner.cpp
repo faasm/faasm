f69e67e1b472d349a0a0bc3c90468e092ce15380
Carlos Segarra <carlossegarragonzalez@gmail.com>
2020-10-12 16:20:17 +0100

    lammps now running on faasm

---
+++ src/runner/lammps_runner.cpp
@@ -0,0 +1,25 @@
+#include <wasm/WasmModule.h>
+
+#include <faabric/util/func.h> // messageFactory
+#include <faabric/util/logging.h>
+
+#include <module_cache/WasmModuleCache.h>
+#include <wamr/WAMRWasmModule.h>
+
+int main()
+{
+    faabric::util::initLogging();
+    auto logger = faabric::util::getLogger();
+    // csegarra: messageFactory(<user>, <function>)
+    auto msg = faabric::util::messageFactory("lammps", "test");
+    msg.set_cmdline("-in faasm://lammps-data/in.viscosity");
+
+    // Create the module
+    module_cache::WasmModuleCache &registry = module_cache::getWasmModuleCache();
+    wasm::WAVMWasmModule &cachedModule = registry.getCachedModule(msg);
+
+    // Create a new module from cache
+    wasm::WAVMWasmModule module(cachedModule);
+    module.execute(msg);
+    return 0;
+}
