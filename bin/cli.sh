36abc6c4d81e1a4cb44fcd4c7c807bc5e9feba30
Carlos Segarra <carlossegarragonzalez@gmail.com>
2020-10-26 16:51:29 +0000

    added some bash scripting to seamlessly attach to a running container instance (rather than spinning a new and different one)

--- bin/cli.sh
+++ bin/cli.sh
@@ -11,14 +11,24 @@ pushd ${PROJ_ROOT} > /dev/null
 VERSION=$(cat VERSION)
 DEFAULT_IMAGE=faasm/cli:${VERSION}
 INNER_SHELL=${SHELL:-"/bin/bash"}
+CLI_NAME="docker_faasm_cli"
 export CLI_IMAGE=${1:-${DEFAULT_IMAGE}}
 echo "Running Faasm CLI (${CLI_IMAGE})"
 
-# Run shell in CLI container
-docker-compose \
-    -f docker/docker-compose-cli.yml \
-    run \
-    cli \
-    ${INNER_SHELL}
+## Run shell in CLI container
+# If already running, attach a new shell to it
+# otherwise, spin a new container
+if [[ $(docker ps -q --filter name=${CLI_NAME}) ]]; then
+    docker exec -it \
+        ${CLI_NAME} \
+        ${INNER_SHELL}
+else
+    docker-compose \
+        -f docker/docker-compose-cli.yml \
+        run \
+        --name ${CLI_NAME} \
+        cli \
+        ${INNER_SHELL}
+fi
 
 popd > /dev/null
