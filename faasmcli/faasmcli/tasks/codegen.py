7e33dcdf7b8e4827d4684a320517a355391e5a43
Simon Shillaker <554768+Shillaker@users.noreply.github.com>
2020-11-12 14:11:31 +0100

    Remove SGD code and smoke test (#344)
    * Remove SGD code and smoke test
    
    * Tidy up codegen python task

--- faasmcli/faasmcli/tasks/codegen.py
+++ faasmcli/faasmcli/tasks/codegen.py
@@ -47,19 +47,12 @@ def local(ctx, wamr=False):
     """
     _do_codegen_user("demo", wamr=wamr)
     _do_codegen_user("errors", wamr=wamr)
-    _do_codegen_user("omp", wamr=wamr)
     _do_codegen_user("mpi", wamr=wamr)
+    _do_codegen_user("omp", wamr=wamr)
+    _do_codegen_user("python", wamr)
 
     # Always run the codegen required by the tests
     codegen(ctx, "demo", "echo", wamr=True)
-
-    # Run these in parallel
-    p = Pool(2)
-    users = [
-        ("python", wamr),
-        ("sgd", wamr),
-    ]
-    p.starmap(_do_codegen_user, users)
 
     print("Running codegen on python shared objects")
     binary = find_codegen_shared_lib()
