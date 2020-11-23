aa65be793face4dd7b74c25048ee21b1547a105a
Simon Shillaker <scs17@ic.ac.uk>
2020-11-23 17:22:05 +0000

    Remove special characters from CPU model

--- bin/print_cpu.sh
+++ bin/print_cpu.sh
@@ -2,4 +2,14 @@
 
 set -e
 
-echo "$(cat /proc/cpuinfo | grep 'model name' | uniq | sed 's/model name.*: //g' | sed 's/ /_/g')"
+CPU_MODEL=$(cat /proc/cpuinfo | grep 'model name' | uniq)
+
+# Strip special characters
+CPU_MODEL=$(echo "$CPU_MODEL" | sed 's/model name.*: //g')
+CPU_MODEL=$(echo "$CPU_MODEL" | sed 's/ /_/g')
+CPU_MODEL=$(echo "$CPU_MODEL" | sed 's/(/_/g')
+CPU_MODEL=$(echo "$CPU_MODEL" | sed 's/)/_/g')
+CPU_MODEL=$(echo "$CPU_MODEL" | sed 's/@/_/g')
+
+# Print the result
+echo $CPU_MODEL
