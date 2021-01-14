d098bcf9dc3164884fd9796722a83f58e689ebd0
Simon Shillaker <scs17@ic.ac.uk>
2021-01-14 17:29:51 +0000

    Dev environment hacks to test ctypes issues

--- func/python/ctype_check.py
+++ func/python/ctype_check.py
@@ -1,10 +1,23 @@
-from ctypes import cdll
+from _ctypes import POINTER
+from _ctypes import _SimpleCData
+from _ctypes import _pointer_type_cache
 
 
-def main():
-    loaded = cdll.LoadLibrary("libfakeLibA.so")
-    print(loaded)
+class c_wchar(_SimpleCData):
+    _type_ = "u"
 
 
-if __name__ == "__main__":
-    main()
+class c_wchar_p(_SimpleCData):
+    _type_ = "Z"
+
+
+def faasm_main():
+    print("Clearing cache")
+    _pointer_type_cache.clear()
+
+    print("Pointer")
+    ptr = POINTER(c_wchar)
+
+    print(ptr)
+
+    return 0
