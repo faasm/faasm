879ff75a7ac467f4b8d3e0427c4699831facb8b0
Carlos Segarra <carlossegarragonzalez@gmail.com>
2021-01-20 09:23:53 +0000

    bump faabric version

--- cmake/ExternalProjects.cmake
+++ cmake/ExternalProjects.cmake
@@ -22,7 +22,7 @@ FetchContent_Declare(wamr_ext
 
 FetchContent_Declare(faabric_ext
     GIT_REPOSITORY "https://github.com/faasm/faabric.git"
-    GIT_TAG "0aa01dd5a0a332dce83e6464583a5b675900bc61"
+    GIT_TAG "origin/standalone-mpi"
     CMAKE_ARGS "-DFAABRIC_BUILD_TESTS=OFF"
 )
 
