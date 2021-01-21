1794a929b3e213a1cf1fc5cff89a860c94ed70fc
Simon Shillaker <scs17@ic.ac.uk>
2021-01-21 17:55:28 +0000

    Fixing up pyhon echo

--- func/python/echo.py
+++ func/python/echo.py
@@ -3,6 +3,11 @@ from pyfaasm.core import get_input_len, read_input, write_output
 
 def faasm_main():
     input_len = get_input_len()
+
+    if input_len == 0:
+        print("Nothing to echo")
+        return 0
+
     i = read_input(input_len)
 
     input_str = i.decode("utf-8")
