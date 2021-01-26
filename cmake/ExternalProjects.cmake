050507f9db091001b1a7ae56a8b89a735f6aa016
David Goltzsche <goltzsche@ibr.cs.tu-bs.de>
2021-01-26 16:00:13 +0100

    update faabric

--- cmake/ExternalProjects.cmake
+++ cmake/ExternalProjects.cmake
@@ -21,7 +21,7 @@ FetchContent_Declare(wamr_ext
 
 FetchContent_Declare(faabric_ext
     GIT_REPOSITORY "https://github.com/dgoltzsche/faabric"
-    GIT_TAG "22ce04844e16c60b5dc497ac78e400ca5fbf5d15"
+    GIT_TAG "edd273f94631b368901d0c26f90e1d47506c876c"
     CMAKE_ARGS "-DFAABRIC_BUILD_TESTS=OFF"
 )
 
