ee94f496c2d2b6f67018cb3a27a9a7b4c22a47db
Simon Shillaker <554768+Shillaker@users.noreply.github.com>
2021-03-26 10:37:44 +0100

    Add CPP toolchain to initial set-up

--- bin/refresh_local.sh
+++ bin/refresh_local.sh
@@ -10,7 +10,9 @@ PROJ_ROOT=${THIS_DIR}/..
 pushd ${PROJ_ROOT} >> /dev/null
 
 FAASM_VERSION=$(cat VERSION)
+CPP_VERSION=$(cat clients/cpp/VERSION)
 IMAGE_TAG=faasm/cli:${FAASM_VERSION}
+CPP_IMAGE_TAG=faasm/cpp-sysroot:${CPP_VERSION}
 
 # This path needs to be absolute with no ..s
 TARGET_DIR=$(pwd)/dev/faasm-local
@@ -24,11 +26,19 @@ docker run -i\
     /bin/bash -c "rm -rf /tmp/localcp/*"
 
 # This command mounts the faasm-local dir from this repo to a temp location,
-# then copies in the contents of /usr/local/faasm
+# then copies in the contents of /usr/local/faasm from the relevant containers
 echo "Populating ${TARGET_DIR} from ${SOURCE_DIR} in ${IMAGE_TAG}"
 docker run -i\
     -v ${TARGET_DIR}:/tmp/localcp \
     ${IMAGE_TAG} \
+    /bin/bash -c "cp -r /usr/local/faasm/* /tmp/localcp/"
+    
+# This command mounts the faasm-local dir from this repo to a temp location,
+# then copies in the contents of /usr/local/faasm
+echo "Populating ${TARGET_DIR} from ${SOURCE_DIR} in ${CPP_IMAGE_TAG}"
+docker run -i\
+    -v ${TARGET_DIR}:/tmp/localcp \
+    ${CPP_IMAGE_TAG} \
     /bin/bash -c "cp -r /usr/local/faasm/* /tmp/localcp/"
 
 popd >> /dev/null
