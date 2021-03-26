fa3b1289ba7d6d5a30754551d6114cfc78ab4900
Simon Shillaker <scs17@ic.ac.uk>
2021-03-26 10:18:02 +0000

    Add python container

--- bin/refresh_local.sh
+++ bin/refresh_local.sh
@@ -19,8 +19,11 @@ pushd ${PROJ_ROOT} >> /dev/null
 
 FAASM_VERSION=$(cat VERSION)
 CPP_VERSION=$(cat clients/cpp/VERSION)
+PY_VERSION=$(cat clients/python/VERSION)
+
 IMAGE_TAG=faasm/cli:${FAASM_VERSION}
 CPP_IMAGE_TAG=faasm/cpp-sysroot:${CPP_VERSION}
+PY_IMAGE_TAG=faasm/cpython:${PY_VERSION}
 
 # This path needs to be absolute with no ..s
 TARGET_DIR=$(pwd)/dev/faasm-local
@@ -37,20 +40,18 @@ else
         /bin/bash -c "rm -rf /tmp/localcp/*"
 fi
 
-# This command mounts the faasm-local dir from this repo to a temp location,
-# then copies in the contents of /usr/local/faasm from the relevant containers
-echo "Populating ${TARGET_DIR} from ${SOURCE_DIR} in ${IMAGE_TAG}"
-docker run -i\
-    -v ${TARGET_DIR}:/tmp/localcp \
-    ${IMAGE_TAG} \
-    /bin/bash -c "cp -r /usr/local/faasm/* /tmp/localcp/"
-    
-# This command mounts the faasm-local dir from this repo to a temp location,
-# then copies in the contents of /usr/local/faasm
-echo "Populating ${TARGET_DIR} from ${SOURCE_DIR} in ${CPP_IMAGE_TAG}"
-docker run -i\
-    -v ${TARGET_DIR}:/tmp/localcp \
-    ${CPP_IMAGE_TAG} \
-    /bin/bash -c "cp -r /usr/local/faasm/* /tmp/localcp/"
+# This function mounts the faasm-local dir from this repo to a temp location,
+# then copies in the contents of /usr/local/faasm from the relevant container
+function pop_local {
+    echo "Populating ${TARGET_DIR} from ${SOURCE_DIR} in $1"
+    docker run -i\
+        -v ${TARGET_DIR}:/tmp/localcp \
+        $1 \
+        /bin/bash -c "cp -r /usr/local/faasm/* /tmp/localcp/"
+}
+
+pop_local ${IMAGE_TAG}
+pop_local ${CPP_IMAGE_TAG}
+pop_local ${PY_IMAGE_TAG}
 
 popd >> /dev/null
