bf0b6225e7909184e64310e6768c1960e842efd9
Carlos Segarra <carlossegarragonzalez@gmail.com>
2021-03-30 09:30:44 +0000

    adapted runner to execute lammps

--- src/runner/func_runner.cpp
+++ src/runner/func_runner.cpp
@@ -51,10 +51,14 @@ int main(int argc, char* argv[])
     }
 
     if (argc > 3) {
-        std::string inputData = argv[3];
-        call.set_inputdata(inputData);
+        // std::string inputData = argv[3];
+        // call.set_inputdata(inputData);
+        int mpi_np = atoi(argv[3]);
+	call.set_cmdline("-in faasm://lammps-data/in.controller");
+	call.set_mpiworldsize(mpi_np);
 
-        logger->info("Adding input data: {}", inputData);
+        logger->info("Adding input file: faasm://lammps-data/in.controller");
+	logger->info("Setting MPI world size: {}", mpi_np);
     }
 
     // Set up a worker pool
