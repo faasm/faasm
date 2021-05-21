4cd4a55429511af2bffe3eddab66e36c035ec19d
Simon Shillaker <scs17@ic.ac.uk>
2021-05-21 08:09:43 +0000

    Clang-tidy config

--- bin/run_clang_tidy.sh
+++ bin/run_clang_tidy.sh
@@ -1,39 +1,49 @@
 #!/bin/bash
 
 set -e
+set -x
+
+THIS_DIR=$(dirname $(readlink -f $0))
+PROJ_ROOT=${THIS_DIR}/..
 
 BUILD_PATH=/build/faasm/
-CONFIG=${FAASM_ROOT}/.clang-tidy
+
+CONFIG_FILE=${PROJ_ROOT}/.clang-tidy
+CONFIG="$(cat $CONFIG_FILE)"
+
 DUMP_OUTPUT=true
 
 # Function to actually run clang-tidy
 function do_tidy {
-    FILES="($@)"
+    FILES="$@"
     run-clang-tidy-10.py \
-        -config '' \
+        -config "${CONFIG}" \
         -j `nproc` \
         -fix \
-        -format \
         -style 'file' \
         -p ${BUILD_PATH} \
         -quiet \
         ${FILES}
 }
 
+MODE="all"
+
 # Support passing a file, directory or nothing
 if [ -f "$1" ]; then
+    MODE="file"
     echo "Running on file $1"
     FILES=($1)
 
     # Assume we want to see output if running on a single file
     unset DUMP_OUTPUT
-
 elif [ -d "$1" ]; then
+    MODE="dir"
     pushd $1 >> /dev/null
     echo "Running on directory $1"
     FILES=$(git ls-files {"*.h","*.cpp","*.c"})
 
 elif [ -z "$1" ]; then
+    MODE="all"
     echo "Running on cwd at $(pwd)"
     FILES=$(git ls-files {func,libs,src,tests}/{"*.h","*.cpp","*.c"})
 
@@ -49,5 +59,7 @@ else
     do_tidy ${FILES[@]} > /dev/null
 fi
 
-popd >> /dev/null || true
+if [ "$MODE" == "dir" ]; then
+    popd >> /dev/null || true
+fi
 
