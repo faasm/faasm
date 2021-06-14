9a24af29a1e110deb3c0b174616a29d99389c777
Simon Shillaker <scs17@ic.ac.uk>
2021-06-14 09:58:17 +0000

    Readded test_maths

---
+++ tests/test/faaslet/test_maths.cpp
@@ -0,0 +1,13 @@
+#include <catch2/catch.hpp>
+
+#include "utils.h"
+
+namespace tests {
+TEST_CASE("Test BLAS support", "[faaslet][!mayfail]")
+{
+    cleanSystem();
+    faabric::Message msg = faabric::util::messageFactory("demo", "blas_check");
+
+    execFunction(msg);
+}
+}
