f25d333d404210ab1dfd310fd6eeb3728a3be192
Simon Shillaker <scs17@ic.ac.uk>
2021-06-17 11:52:29 +0000

    Add test section logging

--- tests/dist/main.cpp
+++ tests/dist/main.cpp
@@ -13,12 +13,25 @@ using namespace faabric::scheduler;
 
 struct LogListener : Catch::TestEventListenerBase
 {
+    // Note, we must call base class versions of overridden methods
+    // https://github.com/catchorg/Catch2/pull/1617
     using TestEventListenerBase::TestEventListenerBase;
 
     void testCaseStarting(Catch::TestCaseInfo const& testInfo) override
     {
-        SPDLOG_INFO("---------------------------------------------");
+        this->Catch::TestEventListenerBase::testCaseStarting(testInfo);
+
+        SPDLOG_INFO("=============================================");
         SPDLOG_INFO("TEST: {}", testInfo.name);
+        SPDLOG_INFO("=============================================");
+    }
+
+    void sectionStarting(Catch::SectionInfo const& sectionInfo) override
+    {
+        this->Catch::TestEventListenerBase::sectionStarting(sectionInfo);
+
+        SPDLOG_INFO("---------------------------------------------");
+        SPDLOG_INFO("SECTION: {}", sectionInfo.name);
         SPDLOG_INFO("---------------------------------------------");
     }
 };
