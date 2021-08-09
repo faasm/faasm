36e9f25fa237e7c17d20be81a5b99a816fe81002
Carlos Segarra <carlos@carlossegarra.com>
2021-08-09 14:53:34 +0000

    fix func disassembly

--- src/runner/func_sym.cpp
+++ src/runner/func_sym.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iomanip>
 
+#include <faabric/transport/context.h>
 #include <faabric/util/files.h>
 #include <faabric/util/func.h>
 #include <faabric/util/logging.h>
@@ -12,40 +13,49 @@
 
 int main(int argc, char* argv[])
 {
-    faabric::util::initLogging();
+    storage::initFaasmS3();
+    faabric::transport::initGlobalMessageContext();
 
-    if (argc < 3) {
-        SPDLOG_ERROR("Must provide user and function name");
-        return 1;
+    // WARNING: All 0MQ-related operations must take place in a self-contined
+    // scope to ensure all sockets are destructed before closing the context.
+    {
+        if (argc < 3) {
+            SPDLOG_ERROR("Must provide user and function name");
+            return 1;
+        }
+
+        std::string user = argv[1];
+        std::string function = argv[2];
+
+        faabric::Message call;
+        call.set_user(user);
+        call.set_function(function);
+
+        SPDLOG_INFO("Disassembling function {}/{}", user, function);
+
+        wasm::WAVMWasmModule module;
+        module.bindToFunction(call);
+
+        std::map<std::string, std::string> disasMap =
+          module.buildDisassemblyMap();
+
+        storage::FileLoader& loader = storage::getFileLoader();
+        std::string outPath = loader.getFunctionSymbolsFile(call);
+        std::ofstream outfile;
+        outfile.open(outPath, std::ios::out);
+
+        for (auto const& p : disasMap) {
+            // Separator needs to be something not found in the symbols
+            // themselves
+            outfile << p.first << " = " << p.second << std::endl;
+        }
+
+        outfile.close();
+
+        SPDLOG_INFO("Output at {}", outPath);
     }
 
-    std::string user = argv[1];
-    std::string function = argv[2];
-
-    faabric::Message call;
-    call.set_user(user);
-    call.set_function(function);
-
-    SPDLOG_INFO("Disassembling function {}/{}", user, function);
-
-    wasm::WAVMWasmModule module;
-    module.bindToFunction(call);
-
-    std::map<std::string, std::string> disasMap = module.buildDisassemblyMap();
-
-    storage::FileLoader& loader = storage::getFileLoader();
-    std::string outPath = loader.getFunctionSymbolsFile(call);
-    std::ofstream outfile;
-    outfile.open(outPath, std::ios::out);
-
-    for (auto const& p : disasMap) {
-        // Separator needs to be something not found in the symbols themselves
-        outfile << p.first << " = " << p.second << std::endl;
-    }
-
-    outfile.close();
-
-    SPDLOG_INFO("Output at {}", outPath);
-
+    faabric::transport::closeGlobalMessageContext();
+    storage::shutdownFaasmS3();
     return 0;
 }
