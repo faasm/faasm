ebdbfb7520539c52a8ca7512b5f3dff3a60ac8ec
Simon Shillaker <scs17@ic.ac.uk>
2021-09-21 13:46:01 +0000

    Fix up upload mounts in dist tests

--- deploy/dist-test/build.sh
+++ deploy/dist-test/build.sh
@@ -6,6 +6,8 @@ THIS_DIR=$(dirname $(readlink -f $0))
 export PROJ_ROOT=${THIS_DIR}/../..
 pushd ${PROJ_ROOT} >> /dev/null
 
+export FAASM_BUILD_MOUNT=/build/faasm
+
 # Run the build
 docker-compose \
     run \
--- deploy/dist-test/build_internal.sh
+++ deploy/dist-test/build_internal.sh
@@ -6,10 +6,12 @@ THIS_DIR=$(dirname $(readlink -f $0))
 export PROJ_ROOT=${THIS_DIR}/../..
 pushd ${PROJ_ROOT} > /dev/null
 
-# Run the build
+# Build everything required for the distributed tests
 inv -r faasmcli/faasmcli dev.cmake --build=Debug
-inv -r faasmcli/faasmcli dev.tools
+inv -r faasmcli/faasmcli dev.cc codegen_func
+inv -r faasmcli/faasmcli dev.cc codegen_shared_obj
 inv -r faasmcli/faasmcli dev.cc dist_tests
 inv -r faasmcli/faasmcli dev.cc dist_test_server
+inv -r faasmcli/faasmcli dev.cc upload
 
 popd >> /dev/null
