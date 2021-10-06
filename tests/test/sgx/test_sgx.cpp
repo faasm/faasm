2107b6f885696ca86312e24333ac67219b152001
Simon Shillaker <scs17@ic.ac.uk>
2021-10-06 10:52:16 +0000

    Fixes for test setup

--- tests/test/sgx/test_sgx.cpp
+++ tests/test/sgx/test_sgx.cpp
@@ -1,3 +1,4 @@
+#include "faasm_fixtures.h"
 #include "utils.h"
 
 #include <catch2/catch.hpp>
@@ -7,12 +8,16 @@
 using namespace wasm;
 
 namespace tests {
-TEST_CASE("Test executing hello function with SGX", "[sgx]")
+TEST_CASE_METHOD(FunctionExecTestFixture,
+                 "Test executing hello function with SGX",
+                 "[sgx]")
 {
     executeWithSGX("demo", "hello");
 }
 
-TEST_CASE("Test executing chaining by name with SGX", "[sgx]")
+TEST_CASE_METHOD(FunctionExecTestFixture,
+                 "Test executing chaining by name with SGX",
+                 "[sgx]")
 {
     executeWithSGX("demo", "chain_named_a");
 }
