eb5d272a50db6462d22195ddb8485ebb87208266
Simon Shillaker <554768+Shillaker@users.noreply.github.com>
2021-10-13 12:44:25 +0200

    Add microbench runner (#494)
    * Add executable for mb runner
    
    * Rearrange
    
    * Removed custom snapshotting
    
    * Docs and tests for microbench
    
    * Add a couple of stubbed intrinsics
    
    * Remove accidentally committed files

---
+++ src/runner/MicrobenchRunner.cpp
@@ -0,0 +1,159 @@
+#include <boost/algorithm/string.hpp>
+#include <boost/filesystem.hpp>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <conf/FaasmConfig.h>
+#include <faaslet/Faaslet.h>
+#include <runner/MicrobenchRunner.h>
+#include <storage/FileLoader.h>
+#include <wasm/WasmModule.h>
+#include <wavm/WAVMWasmModule.h>
+
+#include <faabric/runner/FaabricMain.h>
+#include <faabric/scheduler/ExecutorFactory.h>
+#include <faabric/scheduler/Scheduler.h>
+#include <faabric/transport/context.h>
+#include <faabric/util/config.h>
+#include <faabric/util/logging.h>
+#include <faabric/util/timing.h>
+
+using namespace faabric::util;
+
+#define PREFLIGHT_CALLS true
+
+namespace runner {
+
+int doRun(std::ofstream& outFs,
+          const std::string& user,
+          const std::string& function,
+          int nRuns,
+          const std::string& inputData)
+{
+    // Clear out redis
+    faabric::redis::Redis& redis = faabric::redis::Redis::getQueue();
+    redis.flushAll();
+
+    // Set up invocation message
+    std::shared_ptr<faabric::BatchExecuteRequest> req =
+      faabric::util::batchExecFactory(user, function, 1);
+    faabric::Message& msg = req->mutable_messages()->at(0);
+
+    if (user == "python") {
+        msg.set_pythonuser(msg.user());
+        msg.set_pythonfunction(msg.function());
+        msg.set_ispython(true);
+
+        msg.set_user(PYTHON_USER);
+        msg.set_function(PYTHON_FUNC);
+    }
+
+    msg.set_inputdata(inputData);
+
+    // Check files have been uploaded
+    storage::FileLoader& loader = storage::getFileLoader();
+    std::vector<uint8_t> wasmBytes = loader.loadFunctionWasm(msg);
+    if (wasmBytes.empty()) {
+        SPDLOG_ERROR("Could not load wasm for {}/{}. Make sure it's uploaded");
+        return 1;
+    }
+
+    std::vector<uint8_t> objBytes = loader.loadFunctionObjectFile(msg);
+    if (objBytes.empty()) {
+        SPDLOG_ERROR(
+          "Could not load object file for {}/{}. Make sure you've run codegen");
+        return 1;
+    }
+
+    // Create faaslet
+    faaslet::Faaslet f(msg);
+
+    // Preflight if necessary
+    if (PREFLIGHT_CALLS) {
+        f.executeTask(0, 0, req);
+        f.reset(msg);
+    }
+
+    // Main loop
+    for (int r = 0; r < nRuns; r++) {
+        // Execute
+        TimePoint execStart = startTimer();
+        int returnValue = f.executeTask(0, 0, req);
+        long execNanos = getTimeDiffNanos(execStart);
+        float execMicros = float(execNanos) / 1000;
+
+        // Reset
+        TimePoint resetStart = startTimer();
+        f.restore(msg);
+        long resetNanos = getTimeDiffNanos(resetStart);
+        float resetMicros = float(resetNanos) / 1000;
+
+        // Write result line
+        outFs << user << "," << function << "," << returnValue << ","
+              << execMicros << "," << resetMicros << std::endl;
+    }
+
+    return 0;
+}
+
+int MicrobenchRunner::execute(const std::string& inFile,
+                              const std::string& outFile)
+{
+    if (!boost::filesystem::exists(inFile)) {
+        SPDLOG_ERROR("Input file does not exist: {}", inFile);
+        return 1;
+    }
+
+    // Set up output file
+    std::ofstream outFs;
+    outFs.open(outFile);
+    outFs << "User,Function,Return value,Execution (us),Reset (us)"
+          << std::endl;
+
+    std::fstream inFs;
+    inFs.open(inFile, std::ios::in);
+
+    if (!inFs.is_open()) {
+        SPDLOG_ERROR("Cannot open input file at {}", inFile);
+        return 1;
+    }
+
+    std::string nextLine;
+    while (getline(inFs, nextLine)) {
+        // Skip empty line
+        boost::algorithm::trim(nextLine);
+        if (nextLine.empty()) {
+            continue;
+        }
+
+        // Split the line on commas
+        std::vector<std::string> lineParts;
+        boost::split(lineParts, nextLine, [](char c) { return c == ','; });
+
+        if (lineParts.size() < 3 || lineParts.size() > 4) {
+            SPDLOG_ERROR("Invalid line: {}", nextLine);
+            return 1;
+        }
+
+        std::string user = lineParts[0];
+        std::string function = lineParts[1];
+        int nRuns = std::stoi(lineParts[2]);
+
+        std::string inputData;
+        if (lineParts.size() == 4) {
+            inputData = lineParts[3];
+        }
+
+        SPDLOG_INFO(
+          "Running {}/{} x{} (input [{}])", user, function, nRuns, inputData);
+
+        doRun(outFs, user, function, nRuns, inputData);
+    }
+
+    outFs.close();
+    inFs.close();
+
+    return 0;
+}
+}
---
+++ tests/test/runner/test_microbench_runner.cpp
@@ -0,0 +1,81 @@
+#include <catch2/catch.hpp>
+
+#include "faasm_fixtures.h"
+
+#include <boost/algorithm/string.hpp>
+#include <boost/filesystem.hpp>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <runner/MicrobenchRunner.h>
+
+#include <faabric/util/config.h>
+#include <faabric/util/files.h>
+
+using namespace faabric::util;
+using namespace runner;
+
+namespace tests {
+
+void checkLine(const std::string& line,
+               const std::string& user,
+               const std::string& function)
+{
+    std::vector<std::string> lineParts;
+    boost::split(lineParts, line, [](char c) { return c == ','; });
+
+    REQUIRE(lineParts.size() == 5);
+    REQUIRE(lineParts[0] == user);
+    REQUIRE(lineParts[1] == function);
+    REQUIRE(lineParts[2] == "0");
+
+    float runTime = std::stof(lineParts[3]);
+    float resetTime = std::stof(lineParts[4]);
+
+    REQUIRE(runTime > 0);
+    REQUIRE(resetTime > 0);
+}
+
+TEST_CASE_METHOD(MultiRuntimeFunctionExecTestFixture,
+                 "Test microbench runner",
+                 "[runner]")
+{
+    // Set up file contents
+    std::string specFile = "/tmp/microbench_in.csv";
+
+    if (boost::filesystem::exists(specFile)) {
+        boost::filesystem::remove(specFile);
+    }
+
+    std::ofstream specFs;
+    specFs.open(specFile);
+
+    specFs << "demo,echo,4,blah" << std::endl;
+    specFs << "demo,hello,3" << std::endl;
+    specFs.close();
+
+    std::string outFile = "/tmp/microbench_out.csv";
+
+    MicrobenchRunner::execute(specFile, outFile);
+
+    std::string result = faabric::util::readFileToString(outFile);
+    std::vector<std::string> lines;
+    boost::split(lines, result, [](char c) { return c == '\n'; });
+
+    REQUIRE(lines.size() == 9);
+
+    REQUIRE(lines.at(0) ==
+            "User,Function,Return value,Execution (us),Reset (us)");
+
+    for (int i = 1; i < 5; i++) {
+        checkLine(lines.at(i), "demo", "echo");
+    }
+
+    for (int i = 5; i < 8; i++) {
+        checkLine(lines.at(i), "demo", "hello");
+    }
+
+    REQUIRE(lines.at(8).empty());
+}
+}
