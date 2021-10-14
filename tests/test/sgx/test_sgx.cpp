a619424f48fb75cfc0d9a5c008ccf501154de929
Simon Shillaker <scs17@ic.ac.uk>
2021-10-14 08:32:30 +0000

    Skip SGX tests

--- tests/test/sgx/test_sgx.cpp
+++ tests/test/sgx/test_sgx.cpp
@@ -7,12 +7,12 @@
 using namespace wasm;
 
 namespace tests {
-TEST_CASE("Test executing hello function with SGX", "[sgx]")
+TEST_CASE("Test executing hello function with SGX", "[.][sgx]")
 {
     executeWithSGX("demo", "hello");
 }
 
-TEST_CASE("Test executing chaining by name with SGX", "[sgx]")
+TEST_CASE("Test executing chaining by name with SGX", "[.][sgx]")
 {
     executeWithSGX("demo", "chain_named_a", 10000);
 }
