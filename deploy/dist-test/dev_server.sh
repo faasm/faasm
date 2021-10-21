e1af96c35770d093932488dde9f849d56ae33141
Simon Shillaker <554768+Shillaker@users.noreply.github.com>
2021-10-21 18:59:27 +0200

    Fix build/faasm faasm/build (#516)

--- deploy/dist-test/dev_server.sh
+++ deploy/dist-test/dev_server.sh
@@ -5,7 +5,7 @@ THIS_DIR=$(dirname $(readlink -f $0))
 PROJ_ROOT=${THIS_DIR}/../..
 pushd ${PROJ_ROOT} > /dev/null
 
-export FAASM_BUILD_MOUNT=/faasm/build
+export FAASM_BUILD_MOUNT=/build/faasm
 export FAASM_LOCAL_MOUNT=/usr/local/faasm
 
 if [[ -z "$1" ]]; then
