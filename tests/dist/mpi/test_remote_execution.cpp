db49d60deba35389d8f26ccfeb8bd94bb986c322
Carlos Segarra <carlos@carlossegarra.com>
2021-11-22 10:20:30 +0000

    adding exec graph checks in dist test

--- tests/dist/mpi/test_remote_execution.cpp
+++ tests/dist/mpi/test_remote_execution.cpp
@@ -12,6 +12,7 @@ TEST_CASE_METHOD(DistTestsFixture,
 {
     // Set up this host's resources
     int nLocalSlots = 2;
+    int mpiWorldSize = 4;
     faabric::HostResources res;
     res.set_slots(nLocalSlots);
     sch.setThisHostResources(res);
@@ -21,7 +22,8 @@ TEST_CASE_METHOD(DistTestsFixture,
       faabric::util::batchExecFactory("mpi", "mpi_bcast", 1);
     faabric::Message& msg = req->mutable_messages()->at(0);
     msg.set_ismpi(true);
-    msg.set_mpiworldsize(4);
+    msg.set_mpiworldsize(mpiWorldSize);
+    msg.set_recordexecgraph(true);
 
     // Call the functions
     sch.callFunctions(req);
@@ -30,5 +32,10 @@ TEST_CASE_METHOD(DistTestsFixture,
     faabric::Message result =
       sch.getFunctionResult(msg.id(), functionCallTimeout);
     REQUIRE(result.returnvalue() == 0);
+
+    // Check exec graph
+    auto execGraph = sch.getFunctionExecGraph(result.id());
+    int numNodes = faabric::scheduler::countExecGraphNodes(execGraph);
+    REQUIRE(numNodes == mpiWorldSize);
 }
 }
