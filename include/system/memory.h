eaa165fc5c95fd8c90ae65a7441c9d162db24aab
Simon Shillaker <mail@simonshillaker.com>
2022-02-14 15:27:27 +0000

    Remove stack check

--- include/system/memory.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Minimum stack size
-#define FAASM_MIN_STACK_SIZE_MB 16
-#define FAASM_MIN_STACK_SIZE_BYTES (FAASM_MIN_STACK_SIZE_MB * 1024L * 1024L)
-
-namespace isolation {
-void checkStackSize();
-}
