543ebc0dced5addf89cbac0182aae6133d1720b1
Carlos Segarra <carlos@carlossegarra.com>
2022-02-21 18:07:13 +0000

    enclave: re-factor ocalls to pascal case

--- src/enclave/inside/ecalls.cpp
+++ src/enclave/inside/ecalls.cpp
@@ -17,9 +17,9 @@ extern "C"
     faasm_sgx_status_t ecallInitWamr(void)
     {
         // Initialise WAMR once for all modules
-        ocall_faasm_log_debug("Initialising WAMR runtime");
+        ocallLogDebug("Initialising WAMR runtime");
         if (!wasm::EnclaveWasmModule::initialiseWAMRGlobally()) {
-            ocall_faasm_log_error("Error initialising WAMR globally");
+            ocallLogError("Error initialising WAMR globally");
             return FAASM_SGX_WAMR_RTE_INIT_FAILED;
         }
 
@@ -45,14 +45,14 @@ extern "C"
         {
             std::unique_lock<std::mutex> lock(moduleMapMutex);
             if (moduleMap.find(faaslet_id) != moduleMap.end()) {
-                ocall_faasm_log_error("Faaslet is already bound to a module.");
+                ocallLogError("Faaslet is already bound to a module.");
                 return FAASM_SGX_WAMR_MODULE_LOAD_FAILED;
             }
 
             moduleMap[faaslet_id] = std::make_shared<wasm::EnclaveWasmModule>();
             if (!moduleMap[faaslet_id]->loadWasm(wasm_opcode_ptr,
                                                  wasm_opcode_size)) {
-                ocall_faasm_log_error("Error loading WASM to module");
+                ocallLogError("Error loading WASM to module");
                 return FAASM_SGX_WAMR_MODULE_LOAD_FAILED;
             }
         }
@@ -64,7 +64,7 @@ extern "C"
     {
         std::unique_lock<std::mutex> lock(moduleMapMutex);
         if (moduleMap.find(faaslet_id) == moduleMap.end()) {
-            ocall_faasm_log_error("Faaslet not bound to any module.");
+            ocallLogError("Faaslet not bound to any module.");
             return FAASM_SGX_WAMR_MODULE_NOT_BOUND;
         }
 
@@ -82,7 +82,7 @@ extern "C"
         {
             std::unique_lock<std::mutex> lock(moduleMapMutex);
             if (moduleMap.find(faaslet_id) == moduleMap.end()) {
-                ocall_faasm_log_error("Faaslet not bound to any module.");
+                ocallLogError("Faaslet not bound to any module.");
                 return FAASM_SGX_WAMR_MODULE_NOT_BOUND;
             }
 
@@ -92,7 +92,7 @@ extern "C"
         // Call the function without a lock on the module map, to allow for
         // chaining on the same enclave
         if (!module->callFunction()) {
-            ocall_faasm_log_error("Error trying to call function");
+            ocallLogError("Error trying to call function");
             return FAASM_SGX_WAMR_FUNCTION_UNABLE_TO_CALL;
         }
 
--- src/enclave/inside/memory.cpp
+++ src/enclave/inside/memory.cpp
@@ -4,9 +4,9 @@ namespace sgx {
 static int32_t __sbrk_wrapper(wasm_exec_env_t exec_env, int32_t increment)
 {
     int32_t returnValue;
-    sgx_status_t sgxReturnValue = ocall_sbrk(&returnValue, increment);
+    sgx_status_t sgxReturnValue;
 
-    if ((sgxReturnValue = ocall_sbrk(&returnValue, increment)) != SGX_SUCCESS) {
+    if ((sgxReturnValue = ocallSbrk(&returnValue, increment)) != SGX_SUCCESS) {
         SET_ERROR(FAASM_SGX_OCALL_ERROR(sgxReturnValue));
     }
 
