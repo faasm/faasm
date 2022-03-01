7cff2de113b0b9a8b3f2ebff5e614d0b27f6cbf1
Simon Shillaker <mail@simonshillaker.com>
2022-03-01 09:53:33 +0100

    Update MB runner

--- include/runner/MicrobenchRunner.h
+++ include/runner/MicrobenchRunner.h
@@ -1,3 +1,6 @@
+#include <faabric/proto/faabric.pb.h>
+
+#include <memory>
 #include <string>
 
 namespace runner {
@@ -5,5 +8,16 @@ class MicrobenchRunner
 {
   public:
     static int execute(const std::string& inFile, const std::string& outFile);
+
+    static int doRun(std::ofstream& outFs,
+                     const std::string& user,
+                     const std::string& function,
+                     int nRuns,
+                     const std::string& inputData);
+
+    static std::shared_ptr<faabric::BatchExecuteRequest> createBatchRequest(
+      const std::string& user,
+      const std::string& function,
+      const std::string& inputData);
 };
 }
