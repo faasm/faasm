7a82451ab2e7d5ba6678485b5e7c5024eadc2370
Carlos Segarra <carlos@carlossegarra.com>
2022-03-01 16:17:49 +0000

    enclave/inside: add note on symbol registration

--- include/enclave/inside/native.h
+++ include/enclave/inside/native.h
@@ -30,6 +30,24 @@
     *((uint32_t*)&((AOTModuleInstance*)exec_env->module_inst)                  \
         ->cur_exception[sizeof(_FAASM_SGX_ERROR_PREFIX)]) = (X);
 
+// ---------- WAMR Symbol Registration ----------
+// The function signature to register native symbols is made up of a parenthesis
+// with the type of each input argument, and the type of the return value
+// immediately after. E.g: (ii)i # two int32 input arguments, returns an int32
+// The different supported types are:
+// - i/I,f/F: int32/64 and float32/64
+// - *: the parameter is an offset in WASM memory. **Important** if we use `*`
+//      instead of `i` the runtime automatically translates the address and does
+//      bound checks. Otherwise, we need to translate the address ourselves.
+// - ~: following a `*`, a `~` means that the parameter is the size of the
+//      preceeding pointer (indicated by a `*`). The runtime does the
+//      corresponding bound and address checks.
+// - $: string parameter also with runtime checks.
+//
+// Link to WAMR docs:
+// https://github.com/bytecodealliance/wasm-micro-runtime/blob/main/doc/export_native_api.md#buffer-address-conversion-and-boundary-check
+// ----------------------------------------------
+
 namespace sgx {
 void initialiseSGXWAMRNatives();
 
