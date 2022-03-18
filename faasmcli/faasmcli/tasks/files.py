5d36b58457e3b474b66e0624e66b870d7c2b95ae
Simon Shillaker <mail@simonshillaker.com>
2022-03-18 12:34:25 +0000

    Download task command

--- faasmcli/faasmcli/tasks/files.py
+++ faasmcli/faasmcli/tasks/files.py
@@ -1,22 +1,23 @@
 from invoke import task
-
-from os.path import basename
+from subprocess import run
 
 from faasmcli.util.endpoints import get_upload_host_port
 from faasmcli.util.upload_util import curl_file
 
 
+def get_file_url():
+    host, port = get_upload_host_port()
+    url = "http://{}:{}/file/".format(host, port)
+    return url
+
+
 @task(default=True)
 def upload(ctx, in_path, shared_path):
     """
-    Upload a shared file to Faasm
+    Uploads a shared file
     """
-    host, port = get_upload_host_port()
-
-    url = "http://{}:{}/file/".format(host, port)
-
-    local_filename = basename(in_path)
-    print("Uploading {} to {}".format(local_filename, shared_path))
+    url = get_file_url()
+    print("Uploading {} to {} ({})".format(in_path, shared_path, url))
 
     curl_file(
         url,
@@ -29,6 +30,14 @@ def upload(ctx, in_path, shared_path):
 
 @task
 def download(ctx, shared_path, out_path):
-    host, port = get_upload_host_port()
+    """
+    Downloads a shared file
+    """
+    url = get_file_url()
+    print("Downloading {} to {} ({})".format(shared_path, out_path, url))
 
-    # TODO - implement
+    cmd = ["curl", "-X", "GET", url, "-o", out_path]
+    cmd = " ".join(cmd)
+
+    print(cmd)
+    run(cmd, shell=True, check=True)
