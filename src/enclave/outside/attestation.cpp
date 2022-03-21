2c8d752d31f9010b01b55d11e5f562f95edd9401
Carlos Segarra <carlos@carlossegarra.com>
2022-03-21 18:12:21 +0000

    milestone: quote generation and validation working with a .NET based validation

--- src/enclave/outside/attestation.cpp
+++ src/enclave/outside/attestation.cpp
@@ -1,33 +1,53 @@
-#include <enclave/outside/attestation.h>
 #include <enclave/outside/EnclaveQuote.h>
+#include <enclave/outside/attestation.h>
+#include <enclave/outside/system.h>
 #include <faabric/util/logging.h>
 
-#include <sgx_report.h>
-#include <sgx_ql_lib_common.h>
 #include <sgx_dcap_ql_wrapper.h>
+#include <sgx_ql_lib_common.h>
+#include <sgx_report.h>
 
 namespace sgx {
 
+// An enclave quote is a cryptographic measure of the contents loaded in
+// the enclave. Enclave quotes can be validated through a third-party to
+// attest that code is running in an untampered genuine enclave.
+// Note that, for performance reasons, we could generate the quote as we
+// initialise WAMR and not as a different ECall.
 static EnclaveQuote generateQuote(std::string enclavePath,
                                   std::vector<uint8_t>& enclaveHeldData)
 {
     EnclaveQuote enclaveQuote;
 
     sgx_target_info_t quotingEnclaveTargetInfo;
-    // Returns SGX_QL_INTERFACE_UNAVAILABLE
     quote3_error_t retCode = sgx_qe_get_target_info(&quotingEnclaveTargetInfo);
     if (retCode != SGX_QL_SUCCESS) {
         SPDLOG_ERROR("Error getting the quoting enclave's info: {}", retCode);
         throw std::runtime_error("Error in sgx_qe_get_target_info");
+    } else {
+        SPDLOG_INFO("Success in step 1!");
     }
+
+    // sgx_status_t sgxReturnValue = ecallGenerateQuote(getGlobalEnclaveId(),
+
+    return enclaveQuote;
+}
+
+static bool validateQuote(EnclaveQuote enclaveQuote,
+                          std::string attestationProviderUrl,
+                          bool includeDetails)
+{
+    /*
+     */
 
     /*
     sgx_report_data_t hash;
     sha256sum(&enclaveHeldData[0], enclaveHeldData.size(), hash.d);
 
     sgx_report_t app_report;
-    if(true != create_app_enclave_report(enclavePath.c_str(), qe_target_info, &app_report, &hash)) {
-        throw runtime_error("Call to create_app_report() failed\n");
+    if(true != create_app_enclave_report(enclavePath.c_str(), qe_target_info,
+    &app_report, &hash)) { throw runtime_error("Call to create_app_report()
+    failed\n");
     }
 
     //get quote size
@@ -64,24 +84,22 @@ static EnclaveQuote generateQuote(std::string enclavePath,
     const int hex_buffer_size = 1024*64;
     char hex_buffer[hex_buffer_size];
 
-    enclaveInfo.Type = (int) 2; //for OE, see https://github.com/openenclave/openenclave/blob/3e15573418caed43f9094ff8aec36cdde4f278f7/include/openenclave/bits/types.h#L127
-    enclaveInfo.MrEnclaveHex = format_hex_buffer(hex_buffer, hex_buffer_size, app_report.body.mr_enclave.m, SGX_HASH_SIZE);
-    enclaveInfo.MrSignerHex = format_hex_buffer(hex_buffer, hex_buffer_size, app_report.body.mr_signer.m, SGX_HASH_SIZE);
-    enclaveInfo.ProductIdHex =  uint16_to_buffer(hex_buffer, hex_buffer_size,(uint16_t)app_report.body.isv_prod_id, 16);
+    enclaveInfo.Type = (int) 2; //for OE, see
+    https://github.com/openenclave/openenclave/blob/3e15573418caed43f9094ff8aec36cdde4f278f7/include/openenclave/bits/types.h#L127
+    enclaveInfo.MrEnclaveHex = format_hex_buffer(hex_buffer, hex_buffer_size,
+    app_report.body.mr_enclave.m, SGX_HASH_SIZE); enclaveInfo.MrSignerHex =
+    format_hex_buffer(hex_buffer, hex_buffer_size, app_report.body.mr_signer.m,
+    SGX_HASH_SIZE); enclaveInfo.ProductIdHex =  uint16_to_buffer(hex_buffer,
+    hex_buffer_size,(uint16_t)app_report.body.isv_prod_id, 16);
     enclaveInfo.SecurityVersion = (int) app_report.body.isv_svn;
     enclaveInfo.Attributes = (uint64_t)app_report.body.attributes.flags;
-    enclaveInfo.QuoteHex = format_hex_buffer(hex_buffer, hex_buffer_size, p_quote_buffer, quote_size);
-    enclaveInfo.EnclaveHeldDataHex = format_hex_buffer(hex_buffer, hex_buffer_size, &enclaveHeldData[0], enclaveHeldData.size());
+    enclaveInfo.QuoteHex = format_hex_buffer(hex_buffer, hex_buffer_size,
+    p_quote_buffer, quote_size); enclaveInfo.EnclaveHeldDataHex =
+    format_hex_buffer(hex_buffer, hex_buffer_size, &enclaveHeldData[0],
+    enclaveHeldData.size());
 
     return enclaveInfo;
     */
-    return enclaveQuote;
-}
-
-static bool validateQuote(EnclaveQuote enclaveQuote,
-                          std::string attestationProviderUrl,
-                          bool includeDetails)
-{
     return true;
 }
 
