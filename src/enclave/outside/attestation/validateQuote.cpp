1639e1b1692b8c4b61ee7da8deaad43f20bb8691
Carlos Segarra <carlos@carlossegarra.com>
2022-04-04 18:20:44 +0000

    enclave: quote verification with remote attestation service works with the exception of enclave held data

--- src/enclave/outside/attestation/validateQuote.cpp
+++ src/enclave/outside/attestation/validateQuote.cpp
@@ -1,13 +1,20 @@
 #include <enclave/outside/attestation.h>
+#include <enclave/outside/attestation/AzureAttestationServiceClient.h>
 #include <faabric/util/logging.h>
 
 namespace sgx {
 
-bool validateQuote(const EnclaveInfo& enclaveInfo,
-                   std::string attestationProviderUrl,
+void validateQuote(const EnclaveInfo& enclaveInfo,
+                   const std::string& attestationProviderUrl,
                    bool includeDetails)
 {
-    SPDLOG_ERROR("validateQuote not implemented");
-    return true;
+    AzureAttestationServiceClient client(attestationProviderUrl);
+
+    // TODO - enclave held data not working
+    // Send enclave quote to remote attestation service for validation
+    std::string jwtResponse = client.attestEnclave(enclaveInfo);
+
+    // Validate JWT response token
+    client.validateJwtToken(jwtResponse);
 }
 }
