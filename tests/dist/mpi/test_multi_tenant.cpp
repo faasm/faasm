b18181d33285093fd52bdf89c8c0a29fe248b394
Carlos Segarra <carlos@carlossegarra.com>
2022-05-09 11:09:23 +0000

    dist-test: fix typo in multi-tenant mpi dist test

--- tests/dist/mpi/test_multi_tenant.cpp
+++ tests/dist/mpi/test_multi_tenant.cpp
@@ -32,9 +32,12 @@ TEST_CASE_METHOD(DistTestsFixture,
     // Set up the second message
     std::shared_ptr<faabric::BatchExecuteRequest> reqCopy =
       faabric::util::batchExecFactory("mpi", "mpi_long_alltoall", 1);
-    faabric::Message& msgCopy = req->mutable_messages()->at(0);
+    faabric::Message& msgCopy = reqCopy->mutable_messages()->at(0);
     msgCopy.set_ismpi(true);
     msgCopy.set_mpiworldsize(4);
+
+    // Call the functions for a second time
+    sch.callFunctions(reqCopy);
 
     // Check both results are successful
     faabric::Message result =
