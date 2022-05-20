fe85855bdd36874527dbd127219eebc225b6267a
Carlos Segarra <carlos@carlossegarra.com>
2022-05-20 15:51:07 +0000

    dist-tests: use factored-out method in faabric instead of duplicating code

--- tests/dist/fixtures.h
+++ tests/dist/fixtures.h
@@ -76,7 +76,6 @@ class MpiDistTestsFixture : public DistTestsFixture
         REQUIRE(expectedHosts == hostForRank);
     }
 
-    // TODO - this method is duplicated from faabric's dist tests
     void checkSchedulingFromExecGraph(
       const faabric::scheduler::ExecGraph& execGraph,
       const std::vector<std::string> expectedHostsBefore,
@@ -87,39 +86,9 @@ class MpiDistTestsFixture : public DistTestsFixture
         std::vector<std::string> actualHostsAfter(
           execGraph.rootNode.msg.mpiworldsize());
 
-        std::queue<faabric::scheduler::ExecGraphNode> nodeList;
-        nodeList.push(execGraph.rootNode);
-        while (!nodeList.empty()) {
-            // Process the node at the front
-            auto node = nodeList.front();
-            int returnValue = node.msg.returnvalue();
-            int rank = node.msg.mpirank();
-            std::string executedHost = node.msg.executedhost();
-            if (returnValue == 0) {
-                // We don't know if this particular rank has been migrated or
-                // not. Thus we only write in the before vector if no-one has
-                // written to that rank before
-                if (actualHostsBefore.at(rank).empty()) {
-                    actualHostsBefore.at(rank) = executedHost;
-                }
-                actualHostsAfter.at(rank) = executedHost;
-            } else if (returnValue == MIGRATED_FUNCTION_RETURN_VALUE) {
-                // When we process a message that has been migrated we always
-                // overwrite the contents of the before vector
-                actualHostsBefore.at(rank) = executedHost;
-            } else {
-                SPDLOG_ERROR("Unexpected return value {} for message id {}",
-                             returnValue,
-                             node.msg.id());
-                throw std::runtime_error("Unexpected return value");
-            }
-            nodeList.pop();
-
-            // Add children to the queue
-            for (auto c : node.children) {
-                nodeList.push(c);
-            }
-        }
+        faabric::scheduler::getMigratedMpiRankHostsFromExecGraph(execGraph,
+                                                                 actualHostsBefore,
+                                                                 actualHostsAfter);
 
         REQUIRE(actualHostsBefore == expectedHostsBefore);
         REQUIRE(actualHostsAfter == expectedHostsAfter);
