42e7dace3b112c4c41caf1fad9ff88a1980e4066
Carlos Segarra <carlos@carlossegarra.com>
2022-11-02 17:24:15 +0000

    tests: reduce the number of omp threads for lulesh

--- tests/test/ported_libs/test_lulesh.cpp
+++ tests/test/ported_libs/test_lulesh.cpp
@@ -1,11 +1,16 @@
 #include <catch2/catch.hpp>
 
+#include <conf/FaasmConfig.h>
+
 #include "faasm_fixtures.h"
 #include "utils.h"
 
 namespace tests {
 TEST_CASE_METHOD(FunctionExecTestFixture, "Test executing LULESH", "[libs]")
 {
+    auto conf = faabric::util::getSystemConfig();
+    conf.overrideCpuCount = 10;
+
     std::string cmdLine = "-i 5 -s 20 -r 11 -c 1 -b 1";
     std::string ompThreads = "4";
     faabric::Message msg = faabric::util::messageFactory("lulesh", "main");
