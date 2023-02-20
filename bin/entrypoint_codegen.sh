04b06eaf448035026efcebee0770c6f75362c35e
Carlos Segarra <carlos@carlossegarra.com>
2023-02-20 10:58:56 +0000

    bin: fix entrypoint codegen

--- bin/entrypoint_codegen.sh
+++ bin/entrypoint_codegen.sh
@@ -10,7 +10,7 @@ OBJ_DIR="/usr/local/faasm/object"
 if [[ "${PYTHON_CODEGEN}" == "on" ]]; then
   if [[ ! -f "$OBJ_DIR/python/py_func/function.wasm.o" ]]; then
     echo "Generating python object files"
-    ./bin/codegen_func python py_func
+    ./bin/codegen_func python --func py_func
     ./bin/codegen_shared_obj /usr/local/faasm/runtime_root/lib/python3.8
   fi
 fi
