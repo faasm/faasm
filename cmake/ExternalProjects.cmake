16dea5292b584a92194f2c9c718da9c02a122de3
Carlos <carlos@carlossegarra.com>
2023-03-07 18:06:01 +0000

    Patch LLVM (#729)
    * gh: bump code version
    
    * cpp: bump version
    
    * python: bump version
    
    * docker: just in-case, bump the cpython version
    
    * gha: use docker token instead
    
    * wamr: update hash
    
    * gha: use docker hub token
    
    * cpp, python: bump after merge

--- cmake/ExternalProjects.cmake
+++ cmake/ExternalProjects.cmake
@@ -111,7 +111,7 @@ FetchContent_Declare(wavm_ext
 
 FetchContent_Declare(wamr_ext
     GIT_REPOSITORY "https://github.com/faasm/wasm-micro-runtime"
-    GIT_TAG "135672cab24d877db4ef6933c0ab150351384d51"
+    GIT_TAG "a31e5a4fa299c4f8384f40e157b0a928ad0bda1b"
 )
 
 # WAMR and WAVM both link to LLVM
