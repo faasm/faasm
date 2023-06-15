850730cc92ac7067ca6c6005ef8bed1d73e6e82b
Carlos <carlos@carlossegarra.com>
2023-06-15 15:27:27 +0100

    Turn on code coverage metrics with CodeCov (#759)
    * gha: turn on code coverage
    
    * cli: add flag to tools
    
    * gha: codecov again
    
    * gha: fix again
    
    * gh: cherry-pick nits from faabric pr
    
    * faasmcli: nit
    
    * gha: add note on codecov token

--- faasmcli/faasmcli/tasks/dev.py
+++ faasmcli/faasmcli/tasks/dev.py
@@ -7,7 +7,7 @@ from faasmcli.util.env import (
 from faasmtools.build import FAASM_RUNTIME_ENV_DICT, get_dict_as_cmake_vars
 from invoke import task
 from os import makedirs
-from os.path import exists
+from os.path import exists, join
 from subprocess import run
 
 DEV_TARGETS = [
@@ -32,6 +32,7 @@ def cmake(
     build="Debug",
     perf=False,
     prof=False,
+    coverage=False,
     sanitiser=SANITISER_NONE,
     sgx=FAASM_SGX_MODE_DISABLED,
     cpu=None,
@@ -56,6 +57,7 @@ def cmake(
         "-DCMAKE_C_COMPILER=/usr/bin/clang-13",
         "-DCMAKE_INSTALL_PREFIX={}".format(FAASM_INSTALL_DIR),
         "-DFAASM_PERF_PROFILING=ON" if perf else "",
+        "-DFAASM_CODE_COVERAGE=ON" if coverage else "",
         "-DFAASM_SELF_TRACING=ON" if prof else "",
         "-DFAABRIC_SELF_TRACING=ON" if prof else "",
         "-DFAASM_USE_SANITISER={}".format(sanitiser),
@@ -80,6 +82,7 @@ def tools(
     parallel=0,
     sanitiser=SANITISER_NONE,
     sgx=FAASM_SGX_MODE_DISABLED,
+    coverage=False,
 ):
     """
     Builds all the targets commonly used for development
@@ -87,7 +90,14 @@ def tools(
     if sgx != FAASM_SGX_MODE_DISABLED and sanitiser != SANITISER_NONE:
         raise RuntimeError("SGX and sanitised builds are incompatible!")
 
-    cmake(ctx, clean=clean, build=build, sanitiser=sanitiser, sgx=sgx)
+    cmake(
+        ctx,
+        clean=clean,
+        build=build,
+        sanitiser=sanitiser,
+        sgx=sgx,
+        coverage=coverage,
+    )
 
     targets = " ".join(DEV_TARGETS)
 
@@ -126,3 +136,31 @@ def cc(ctx, target, clean=False, parallel=0):
         shell=True,
         check=True,
     )
+
+
+@task
+def coverage_report(ctx, file_in, file_out):
+    """
+    Generate code coverage report
+    """
+    tmp_file = "tmp_gha.profdata"
+
+    # First, merge in the raw profiling data
+    llvm_cmd = [
+        "llvm-profdata-13",
+        "merge -sparse {}".format(file_in),
+        "-o {}".format(tmp_file),
+    ]
+    llvm_cmd = " ".join(llvm_cmd)
+    run(llvm_cmd, shell=True, check=True, cwd=PROJ_ROOT)
+
+    # Second, generate the coverage report
+    llvm_cmd = [
+        "llvm-cov-13 show",
+        "--ignore-filename-regex=/usr/local/code/faasm/tests/*",
+        join(FAASM_BUILD_DIR, "bin", "tests"),
+        "-instr-profile={}".format(tmp_file),
+        "> {}".format(file_out),
+    ]
+    llvm_cmd = " ".join(llvm_cmd)
+    run(llvm_cmd, shell=True, check=True, cwd=PROJ_ROOT)
