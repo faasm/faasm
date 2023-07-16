9820db77b256f8d3ab15168b8c2394c0164b091c
Carlos Segarra <carlos@carlossegarra.com>
2023-07-16 10:28:47 +0000

    dist-tests: use faasmctl to run dist-tests in gha

--- deploy/dist-test/run.sh
+++ deploy/dist-test/run.sh
@@ -4,25 +4,10 @@ THIS_DIR=$(dirname $(readlink -f $0))
 export PROJ_ROOT=${THIS_DIR}/../..
 pushd ${PROJ_ROOT} > /dev/null
 
-export FAASM_BUILD_MOUNT=/build/faasm
-export FAASM_CODE_MOUNT=/usr/local/code/faasm
-export FAASM_CONAN_MOUNT=/root/.conan
-export PLANNER_BUILD_MOUNT=${FAASM_BUILD_MOUNT}
-
 RETURN_VAL=0
 
-# Run the test server in the background
-docker compose \
-    up \
-    -d \
-    dist-test-server
-
-# Run the tests
-docker compose \
-    run \
-    --rm \
-    faasm-cli \
-    /build/faasm/bin/dist_tests
+# Run the distributed tests
+faasmctl cli.faasm --cmd "/build/faasm/bin/dist_tests"
 
 RETURN_VAL=$?
 
@@ -30,9 +15,7 @@ echo "-------------------------------------------"
 echo "                SERVER LOGS                "
 echo "-------------------------------------------"
 docker compose logs dist-test-server
-
-# Stop everything
-docker compose stop upload dist-test-server
+faasmctl logs -s dist-test-server
 
 popd >> /dev/null
 
