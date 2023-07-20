2af450e17d1dbfbf28c53b354a0df86b456db22a
Carlos <carlos@carlossegarra.com>
2023-07-20 16:47:03 +0100

    Use Semantic Versioning (#775)
    * git: use semver and add task to bump faasmctl
    
    * gh: bump cpp, python, and faabric submodule
    
    * cpp: bump version
    
    * python: bump version
    
    * faabric: bump tracked version
    
    * faasmctl: bump tracked version
    
    * gh: bump minor version
    
    * faabric: bump
    
    * gha: update azure integration tests
    
    * gha: more aks fixes
    
    * faasmctl: bump to latest version
    
    * gh: bump clients after merge

--- faasmcli/faasmcli/util/version.py
+++ faasmcli/faasmcli/util/version.py
@@ -9,13 +9,13 @@ def get_version(project="faasm"):
             version = version.strip()
         return version
 
-    def read_version_from_env_file(env_file, var_name):
-        with open(env_file, "r") as fh:
+    def read_version_from_file_path(file_path, var_name):
+        with open(file_path, "r") as fh:
             lines = fh.readlines()
             lines = [line.strip() for line in lines if var_name in line]
             if len(lines) != 1:
                 raise RuntimeError("Inconsistent env. file state")
-            return lines[0].split("=")[1]
+            return lines[0].split("=")[-1]
 
     if project == "faasm":
         ver_file = join(PROJ_ROOT, "VERSION")
@@ -23,22 +23,27 @@ def get_version(project="faasm"):
 
     env_file = join(PROJ_ROOT, ".env")
     if project == "cpp":
-        old_ver = read_version_from_env_file(env_file, "CPP_VERSION")
+        old_ver = read_version_from_file_path(env_file, "CPP_VERSION")
         new_ver_file = join(PROJ_ROOT, "clients", "cpp", "VERSION")
         new_ver = read_version_from_file(new_ver_file)
         return old_ver, new_ver
 
     if project == "python":
-        old_ver = read_version_from_env_file(env_file, "PYTHON_VERSION")
+        old_ver = read_version_from_file_path(env_file, "PYTHON_VERSION")
         new_ver_file = join(PROJ_ROOT, "clients", "python", "VERSION")
         new_ver = read_version_from_file(new_ver_file)
         return old_ver, new_ver
 
     if project == "faabric":
-        old_ver = read_version_from_env_file(env_file, "FAABRIC_VERSION")
+        old_ver = read_version_from_file_path(env_file, "FAABRIC_VERSION")
         new_ver_file = join(PROJ_ROOT, "faabric", "VERSION")
         new_ver = read_version_from_file(new_ver_file)
         return old_ver, new_ver
+
+    if project == "faasmctl":
+        reqs_file = join(PROJ_ROOT, "faasmcli", "requirements.txt")
+        old_ver = read_version_from_file_path(reqs_file, "faasmctl")
+        return old_ver
 
     print("Unrecognised project name to get version from: {}".format(project))
     raise RuntimeError("Unrecognised project name")
