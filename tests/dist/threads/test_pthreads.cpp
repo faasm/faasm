27820f437bdba5a5b6430771889514585f6576ee
Carlos Segarra <carlos@carlossegarra.com>
2023-09-05 16:14:08 +0000

    nits: run clang format

--- tests/dist/threads/test_pthreads.cpp
+++ tests/dist/threads/test_pthreads.cpp
@@ -11,7 +11,6 @@ TEST_CASE_METHOD(DistTestsFixture, "Test pthreads across hosts", "[scheduler]")
 {
     conf.overrideCpuCount = 6;
 
-
     // TODO(wamr-omp)
     if (faasmConf.wasmVm == "wamr") {
         return;
