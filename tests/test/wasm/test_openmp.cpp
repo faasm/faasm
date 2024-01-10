54c52179d5e6efed4a27a4bb983761eb136e283a
Carlos Segarra <carlos@carlossegarra.com>
2024-01-10 14:23:54 +0000

    tests(openmp): set singlehost flag in the request

--- tests/test/wasm/test_openmp.cpp
+++ tests/test/wasm/test_openmp.cpp
@@ -37,6 +37,7 @@ class OpenMPTestFixture
     {
         faabric::Message msg = faabric::util::messageFactory("omp", function);
         auto req = faabric::util::batchExecFactory("omp", function, 1);
+        req->set_singlehost(true);
         faabric::Message result =
           executeWithPool(req, OMP_TEST_TIMEOUT_MS).at(0);
 
