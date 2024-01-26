0ad88e26cc030ab66e619dd4e533dffac43187db
Carlos <carlos@carlossegarra.com>
2024-01-26 18:46:00 +0000

    runner: resurrect func runner (#824)

--- src/runner/func_runner.cpp
+++ src/runner/func_runner.cpp
@@ -1,17 +1,10 @@
-#include <conf/FaasmConfig.h>
-#include <faabric/executor/ExecutorFactory.h>
-#include <faabric/planner/PlannerClient.h>
-#include <faabric/redis/Redis.h>
-#include <faabric/runner/FaabricMain.h>
+#include <faabric/executor/ExecutorContext.h>
 #include <faabric/util/batch.h>
-#include <faabric/util/config.h>
-#include <faabric/util/environment.h>
 #include <faabric/util/logging.h>
 #include <faabric/util/timing.h>
 #include <faaslet/Faaslet.h>
 #include <runner/runner_utils.h>
 #include <storage/FileLoader.h>
-#include <wasm/WasmModule.h>
 
 namespace po = boost::program_options;
 
@@ -26,27 +19,6 @@ int doRunner(int argc, char* argv[])
     std::shared_ptr<faabric::BatchExecuteRequest> req =
       faabric::util::batchExecFactory(user, function, 1);
     faabric::Message& msg = req->mutable_messages()->at(0);
-
-    faabric::util::SystemConfig& conf = faabric::util::getSystemConfig();
-    conf::FaasmConfig& faasmConf = conf::getFaasmConfig();
-
-    // Set timeout to ensure longer functions can finish
-    conf.boundTimeout = 120000;
-    conf.globalMessageTimeout = 120000;
-    faasmConf.chainedCallTimeout = 120000;
-
-    // Make sure we have enough space for chained calls
-    int nThreads = std::min<int>(faabric::util::getUsableCores(), 10);
-    faabric::HostResources res;
-    res.set_slots(nThreads);
-    faabric::scheduler::Scheduler& sch = faabric::scheduler::getScheduler();
-    sch.setThisHostResources(res);
-
-    conf.defaultMpiWorldSize = 5;
-
-    // Clear out redis
-    faabric::redis::Redis& redis = faabric::redis::Redis::getQueue();
-    redis.flushAll();
 
     if (user == "python") {
         msg.set_pythonuser(msg.user());
@@ -75,29 +47,27 @@ int doRunner(int argc, char* argv[])
                     vm["cmdline"].as<std::string>());
     }
 
-    // Set up the system
-    auto fac = std::make_shared<faaslet::FaasletFactory>();
-    faabric::runner::FaabricMain m(fac);
-    m.startRunner();
+    // Create a Faaslet and set the executor context
+    faabric::executor::ExecutorContext::set(nullptr, req, 0);
+    faaslet::Faaslet f(msg);
 
     // Submit the invocation
     PROF_START(FunctionExec)
-    auto& plannerCli = faabric::planner::getPlannerClient();
-    plannerCli.callFunctions(req);
-
-    // Await the result
-    const faabric::Message& result =
-      plannerCli.getMessageResult(msg, conf.globalMessageTimeout);
-    if (result.returnvalue() != 0) {
-        SPDLOG_ERROR("Execution failed: {}", result.outputdata());
-        throw std::runtime_error("Executing function failed");
-    }
-
+    int returnValue = f.executeTask(0, 0, req);
     PROF_END(FunctionExec)
 
-    m.shutdown();
+    f.reset(msg);
+    f.shutdown();
 
-    return 0;
+    SPDLOG_INFO("Finished running function {}/{} (exit code: {})",
+                user,
+                function,
+                returnValue);
+    if (!msg.outputdata().empty()) {
+        SPDLOG_INFO("Function output: {}", msg.outputdata());
+    }
+
+    return returnValue;
 }
 
 int main(int argc, char* argv[])
@@ -106,8 +76,6 @@ int main(int argc, char* argv[])
 
     PROF_BEGIN
 
-    // WARNING: All 0MQ-related operations must take place in a self-contined
-    // scope to ensure all sockets are destructed before closing the context.
     int result = doRunner(argc, argv);
 
     PROF_SUMMARY
