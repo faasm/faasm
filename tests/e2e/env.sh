5292d94f41cba64e850e9bd6597ed838cd500890
Carlos Segarra <carlos@carlossegarra.com>
2024-02-02 11:12:41 +0000

    tests(e2e): get FAASM and faasmctl version from checked-out code

--- tests/e2e/env.sh
+++ tests/e2e/env.sh
@@ -1,6 +1,9 @@
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
+PROJ_ROOT="${THIS_DIR}/../.."
+
 # Versions
-export FAASM_VERSION=0.10.2
-export FAASMCTL_VERSION=0.9.1
+export FAASM_VERSION=$(cat ${PROJ_ROOT}/VERSION)
+export FAASMCTL_VERSION=$(cat ${PROJ_ROOT}/requirements.txt | awk -F'faasmctl==' '{ print $2 }' | awk 'NF')
 
 # Faasmctl vars
 export FAASM_INI_FILE=${E2E_TESTS_ROOT}/faasm.ini
