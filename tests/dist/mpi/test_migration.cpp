9e4c8215ec190e6ef7ce897d9e64107d5ee812a4
Carlos Segarra <carlos@carlossegarra.com>
2024-04-22 16:24:55 +0000

    faabric: bump to add planner's eviction policy

--- tests/dist/mpi/test_migration.cpp
+++ tests/dist/mpi/test_migration.cpp
@@ -5,8 +5,6 @@
 #include <faabric/scheduler/Scheduler.h>
 #include <faabric/util/batch.h>
 
-#include <set>
-
 namespace tests {
 
 TEST_CASE_METHOD(MpiDistTestsFixture,
@@ -81,6 +79,84 @@ TEST_CASE_METHOD(MpiDistTestsFixture,
 
     // Check it's successful
     checkMpiBatchResults(req, hostsAfter);
+}
+
+TEST_CASE_METHOD(MpiDistTestsFixture,
+                 "Test migrating an MPI application with Compact policy",
+                 "[mpi]")
+{
+    updatePlannerPolicy("compact");
+
+    // Allocate resources so that two mpi ranks are scheduled in one worker
+    // and two in the other
+    int worldSize = 4;
+    // Give enough slots for the application to run in any of the two hosts
+    setLocalRemoteSlots(2 * worldSize, worldSize, worldSize, 0);
+
+    // Set up the message
+    std::shared_ptr<faabric::BatchExecuteRequest> req =
+      faabric::util::batchExecFactory("mpi", "migrate", 1);
+    faabric::Message& msg = req->mutable_messages()->at(0);
+    msg.set_ismpi(true);
+    msg.set_mpiworldsize(worldSize);
+
+    // Try to migrate at 50% of execution
+    int numLoops = 10000;
+    int checkAt = 5;
+    msg.set_cmdline(fmt::format("{} {}", checkAt, numLoops));
+
+    // Before calling the function, pre-load a scheduling decision
+    auto preloadDec = std::make_shared<batch_scheduler::SchedulingDecision>(
+      req->appid(), req->groupid());
+
+    std::vector<std::string> hostsBefore;
+    std::vector<std::string> hostsAfter;
+
+    SECTION("Do not migrate main rank")
+    {
+        hostsBefore = { getDistTestMasterIp(),
+                        getDistTestMasterIp(),
+                        getDistTestWorkerIp(),
+                        getDistTestWorkerIp() };
+        hostsAfter = { getDistTestMasterIp(),
+                       getDistTestMasterIp(),
+                       getDistTestMasterIp(),
+                       getDistTestMasterIp() };
+    }
+
+    SECTION("Migrate main rank")
+    {
+        hostsBefore = { getDistTestWorkerIp(),
+                        getDistTestWorkerIp(),
+                        getDistTestMasterIp(),
+                        getDistTestMasterIp() };
+        hostsAfter = { getDistTestMasterIp(),
+                       getDistTestMasterIp(),
+                       getDistTestMasterIp(),
+                       getDistTestMasterIp() };
+    }
+
+    assert(hostsBefore.size() == worldSize);
+    assert(hostsAfter.size() == worldSize);
+
+    // In a preloaded scheduling decision we only care about the host we
+    // execute at, and the group index
+    for (int i = 0; i < hostsBefore.size(); i++) {
+        preloadDec->addMessage(hostsBefore.at(i), 0, 0, i);
+    }
+
+    // Preload decision
+    plannerCli.preloadSchedulingDecision(preloadDec);
+
+    // Call the functions
+    plannerCli.callFunctions(req);
+    auto actualHostsBefore = waitForMpiMessagesInFlight(req);
+    REQUIRE(actualHostsBefore == hostsBefore);
+
+    // Check it's successful
+    checkMpiBatchResults(req, hostsAfter);
+
+    updatePlannerPolicy("bin-pack");
 }
 
 TEST_CASE_METHOD(MpiDistTestsFixture,
@@ -162,7 +238,7 @@ TEST_CASE_METHOD(MpiDistTestsFixture,
 }
 
 TEST_CASE_METHOD(MpiDistTestsFixture,
-                 "Test migrating two concurrent MPI applications",
+                 "Test migrating two concurrent MPI applications (bin-pack)",
                  "[mpi]")
 {
     int worldSize = 4;
@@ -229,5 +305,79 @@ TEST_CASE_METHOD(MpiDistTestsFixture,
     // Check it's successful
     checkMpiBatchResults(reqA, expectedHostsAfterA);
     checkMpiBatchResults(reqB, expectedHostsAfterB);
+}
+
+TEST_CASE_METHOD(MpiDistTestsFixture,
+                 "Test migrating with concurrent MPI applications (compact)",
+                 "[mpi]")
+{
+    updatePlannerPolicy("compact");
+
+    int worldSize = 4;
+    int numLoops = 10000;
+    int checkAt = 5;
+
+    // Request A will migrate
+    std::shared_ptr<faabric::BatchExecuteRequest> reqA =
+      faabric::util::batchExecFactory("mpi", "migrate", 1);
+    reqA->mutable_messages(0)->set_ismpi(true);
+    reqA->mutable_messages(0)->set_mpiworldsize(worldSize);
+    reqA->mutable_messages(0)->set_cmdline(
+      fmt::format("{} {}", checkAt, numLoops));
+
+    // Request B will not migrate
+    std::shared_ptr<faabric::BatchExecuteRequest> reqB =
+      faabric::util::batchExecFactory("mpi", "mpi_long_alltoall", 1);
+    reqB->mutable_messages(0)->set_ismpi(true);
+    reqB->mutable_messages(0)->set_mpiworldsize(worldSize);
+    reqB->mutable_messages(0)->set_cmdline(
+      fmt::format("{} {}", numLoops, numLoops));
+
+    // Allocate enough resources to run both applications in just one host.
+    // We will still migrate one app to increase the number of free hosts
+    setLocalRemoteSlots(2 * worldSize, 2 * worldSize, 0, 0);
+
+    std::vector<std::string> expectedHostsBeforeA = { getDistTestMasterIp(),
+                                                      getDistTestMasterIp(),
+                                                      getDistTestMasterIp(),
+                                                      getDistTestMasterIp() };
+
+    std::vector<std::string> expectedHostsBeforeB = { getDistTestWorkerIp(),
+                                                      getDistTestWorkerIp(),
+                                                      getDistTestWorkerIp(),
+                                                      getDistTestWorkerIp() };
+    assert(expectedHostsBeforeA.size() == expectedHostsBeforeB.size());
+    assert(expectedHostsBeforeA.size() == worldSize);
+
+    // Preload scheduling decisions so that both applications are scheduled
+    // in the same way:
+    auto preloadDecA = std::make_shared<batch_scheduler::SchedulingDecision>(
+      reqA->appid(), reqA->groupid());
+    auto preloadDecB = std::make_shared<batch_scheduler::SchedulingDecision>(
+      reqB->appid(), reqB->groupid());
+    for (int i = 0; i < worldSize; i++) {
+        preloadDecA->addMessage(expectedHostsBeforeA.at(i), 0, 0, i);
+        preloadDecB->addMessage(expectedHostsBeforeB.at(i), 0, 0, i);
+    }
+    plannerCli.preloadSchedulingDecision(preloadDecA);
+    plannerCli.preloadSchedulingDecision(preloadDecB);
+
+    plannerCli.callFunctions(reqB);
+    auto actualHostsBeforeB = waitForMpiMessagesInFlight(reqB);
+    REQUIRE(actualHostsBeforeB == expectedHostsBeforeB);
+
+    plannerCli.callFunctions(reqA);
+    auto actualHostsBeforeA = waitForMpiMessagesInFlight(reqA);
+    REQUIRE(actualHostsBeforeA == expectedHostsBeforeA);
+
+    std::vector<std::string> expectedHostsAfterA(worldSize,
+                                                 getDistTestWorkerIp());
+    std::vector<std::string> expectedHostsAfterB = expectedHostsBeforeB;
+
+    // Check it's successful
+    checkMpiBatchResults(reqA, expectedHostsAfterA);
+    checkMpiBatchResults(reqB, expectedHostsAfterB);
+
+    updatePlannerPolicy("bin-pack");
 }
 }
