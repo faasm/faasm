e310c911832f91d2e3b65a486fcdab2d36747db8
Carlos Segarra <carlos@carlossegarra.com>
2024-07-26 11:52:56 +0000

    tests: update chaining test

--- tests/test/faaslet/test_chaining.cpp
+++ tests/test/faaslet/test_chaining.cpp
@@ -41,4 +41,17 @@ TEST_CASE_METHOD(MultiRuntimeFunctionExecTestFixture,
     auto req = faabric::util::batchExecFactory("demo", "chain_named_a", 1);
     executeWithPool(req, 5000);
 }
+
+TEST_CASE_METHOD(MultiRuntimeFunctionExecTestFixture,
+                 "Test function chaining and waiting for output",
+                 "[faaslet]")
+{
+    SECTION("WAMR")
+    {
+        faasmConf.wasmVm = "wamr";
+    }
+
+    auto req = faabric::util::batchExecFactory("demo", "chain_output", 1);
+    executeWithPool(req, 5000);
+}
 }
