7690ed4ab985625731d4e466801b21820fa71632
Carlos Segarra <carlos@carlossegarra.com>
2024-08-01 10:08:26 +0000

    gha: more fixes to sgx-hw

--- src/enclave/inside/funcs.cpp
+++ src/enclave/inside/funcs.cpp
@@ -60,22 +60,21 @@ static unsigned int faasm_chain_ptr_wrapper(wasm_exec_env_t execEnv,
                                             const uint8_t* inputData,
                                             unsigned int inputSize)
 {
-    // 01/07/2021 - Chain function by pointer is not supported in SGX as it
-    // breaks attestation model. Chain by name instead.
-    SET_ERROR(FAASM_SGX_WAMR_FUNCTION_NOT_IMPLEMENTED);
-
-    return 1;
+    UNIMPLEMENTED_WASM_INTRINSIC("faasm_chain_ptr");
 }
 
 static unsigned int faasm_await_call_wrapper(wasm_exec_env_t execEnv,
                                              unsigned int callId)
 {
+    SPDLOG_DEBUG_SGX("S - faasm_wait_call %i", callId);
+
     sgx_status_t sgxReturnValue;
     unsigned int returnValue;
     if ((sgxReturnValue = ocallFaasmAwaitCall(&returnValue, callId)) !=
         SGX_SUCCESS) {
         SET_ERROR(FAASM_SGX_OCALL_ERROR(sgxReturnValue));
     }
+
     return returnValue;
 }
 
@@ -84,16 +83,19 @@ static unsigned int faasm_await_call_output_wrapper(wasm_exec_env_t execEnv,
                                                     char* buffer,
                                                     unsigned int bufferSize)
 {
+    SPDLOG_DEBUG_SGX("S - faasm_wait_call_output %i", callId);
+
     sgx_status_t sgxReturnValue;
     unsigned int returnValue;
     if ((sgxReturnValue = ocallFaasmAwaitCallOutput(
            &returnValue, callId, buffer, bufferSize)) != SGX_SUCCESS) {
         SET_ERROR(FAASM_SGX_OCALL_ERROR(sgxReturnValue));
     }
+
     return returnValue;
 }
 
-static NativeSymbol ns[] = {
+static NativeSymbol funcsNs[] = {
     REG_FAASM_NATIVE_FUNC(faasm_read_input, "($i)i"),
     REG_FAASM_NATIVE_FUNC(faasm_write_output, "($i)"),
     REG_FAASM_NATIVE_FUNC(faasm_chain_name, "($$i)i"),
@@ -104,7 +106,7 @@ static NativeSymbol ns[] = {
 
 uint32_t getFaasmFunctionsApi(NativeSymbol** nativeSymbols)
 {
-    *nativeSymbols = ns;
-    return sizeof(ns) / sizeof(NativeSymbol);
+    *nativeSymbols = funcsNs;
+    return sizeof(funcsNs) / sizeof(NativeSymbol);
 }
 }
