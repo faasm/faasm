145b7d776b38a9620b7205c44a1cfac6b5d48d77
Carlos Segarra <carlos@carlossegarra.com>
2024-09-04 10:59:48 +0000

    upload: fix upload_server compilation

--- src/upload/upload_server.cpp
+++ src/upload/upload_server.cpp
@@ -1,11 +1,12 @@
 #include <storage/S3Wrapper.h>
 #include <upload/UploadEndpointHandler.h>
-#include <upload/UploadServer.h>
 
 #include <faabric/endpoint/FaabricEndpoint.h>
 #include <faabric/state/StateServer.h>
 #include <faabric/util/config.h>
 #include <faabric/util/logging.h>
+
+static const int NUM_SERVER_THREADS = 4;
 
 int main()
 {
@@ -23,9 +24,9 @@ int main()
     // Start the upload server in the main thread
     SPDLOG_INFO("Starting upload endpoint");
     faabric::endpoint::FaabricEndpoint endpoint(
-      UPLOAD_PORT,
-      2, // TODO
-      std::make_shared<upload::UploadEndpoingHandler>());
+      std::atoi(UPLOAD_PORT),
+      NUM_SERVER_THREADS,
+      std::make_shared<upload::UploadEndpointHandler>());
     endpoint.start(faabric::endpoint::EndpointMode::SIGNAL);
 
     // Stop the state server
