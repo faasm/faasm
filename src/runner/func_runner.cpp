ad301c4036f165037f607f7492947ad0fd11e907
Carlos Segarra <carlos@carlossegarra.com>
2024-10-19 16:08:05 +0000

    runner: minor changes

--- src/runner/func_runner.cpp
+++ src/runner/func_runner.cpp
@@ -12,9 +12,9 @@ int doRunner(int argc, char* argv[])
 {
     faabric::util::initLogging();
 
-    auto vm = runner::parseRunnerCmdLine(argc, argv);
-    std::string user = vm["user"].as<std::string>();
-    std::string function = vm["function"].as<std::string>();
+    auto vmCmd = runner::parseRunnerCmdLine(argc, argv);
+    std::string user = vmCmd["user"].as<std::string>();
+    std::string function = vmCmd["function"].as<std::string>();
 
     std::shared_ptr<faabric::BatchExecuteRequest> req =
       faabric::util::batchExecFactory(user, function, 1);
@@ -35,29 +35,29 @@ int doRunner(int argc, char* argv[])
         SPDLOG_INFO("Running function {}/{}", user, function);
     }
 
-    if (vm.count("input-data")) {
-        msg.set_inputdata(vm["input-data"].as<std::string>());
+    if (vmCmd.count("input-data")) {
+        msg.set_inputdata(vmCmd["input-data"].as<std::string>());
         SPDLOG_INFO("Adding input data: {}",
-                    vm["input-data"].as<std::string>());
+                    vmCmd["input-data"].as<std::string>());
     }
 
-    if (vm.count("cmdline")) {
-        msg.set_cmdline(vm["cmdline"].as<std::string>());
+    if (vmCmd.count("cmdline")) {
+        msg.set_cmdline(vmCmd["cmdline"].as<std::string>());
         SPDLOG_INFO("Adding command line arguments: {}",
-                    vm["cmdline"].as<std::string>());
+                    vmCmd["cmdline"].as<std::string>());
     }
 
     // Create a Faaslet and set the executor context
     faabric::executor::ExecutorContext::set(nullptr, req, 0);
-    faaslet::Faaslet f(msg);
+    faaslet::Faaslet faaslet(msg);
 
     // Submit the invocation
     PROF_START(FunctionExec)
-    int returnValue = f.executeTask(0, 0, req);
+    int returnValue = faaslet.executeTask(0, 0, req);
     PROF_END(FunctionExec)
 
-    f.reset(msg);
-    f.shutdown();
+    faaslet.reset(msg);
+    faaslet.shutdown();
 
     SPDLOG_INFO("Finished running function {}/{} (exit code: {})",
                 user,
@@ -81,5 +81,6 @@ int main(int argc, char* argv[])
     PROF_SUMMARY
 
     storage::shutdownFaasmS3();
+
     return result;
 }
