e1976187aaf0b2bb79243e23e9fb63b0dfb1d331
Carlos Segarra <carlos@carlossegarra.com>
2025-04-23 14:50:52 +0000

    tests: more cleanup

--- tests/utils/faasm_fixtures.h
+++ tests/utils/faasm_fixtures.h
@@ -184,7 +184,8 @@ class FunctionLoaderTestFixture : public S3TestFixture
         faasmConf.wasmVm = oldWasmVm;
 
         // Use a shared object we know exists
-        /*
+        /* 23/04/2025 - Shared-object codegen disabled until python support
+         * is resurrected.
         localSharedObjFile =
           faasmConf.runtimeFilesDir + "/lib/python3.8/lib-dynload/syslog.so";
         sharedObjWasm = faabric::util::readFileToBytes(localSharedObjFile);
